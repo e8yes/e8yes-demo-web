b138b8ead24533fff766b1222efb99496bd41f8b
Davis Wen <daviesx66@gmail.com>
2019-04-11 00:44:29 -0700

    Added mybatis dependency and sql session builder.

---
+++ server/src/main/java/org/e8yes/srvs/dao/DatabaseConnection.java
@@ -0,0 +1,46 @@
+package org.e8yes.srvs.dao;
+
+import javax.sql.DataSource;
+import org.apache.ibatis.datasource.pooled.PooledDataSource;
+import org.apache.ibatis.mapping.Environment;
+import org.apache.ibatis.session.Configuration;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.apache.ibatis.transaction.jdbc.JdbcTransactionFactory;
+import org.e8yes.srvs.dao.mappers.AUserMapper;
+
+/**
+ *
+ * @author daviswen
+ */
+class Database {
+
+        private static SqlSessionFactory fact;
+
+        private static void
+                init() {
+                DataSource dataSource = new PooledDataSource("org.postgresql.Driver",
+                                                             "jdbc:postgresql://localhost:5432/demoweb",
+                                                             "postgres",
+                                                             "password");
+
+                Environment environment = new Environment("Development",
+                                                          new JdbcTransactionFactory(),
+                                                          dataSource);
+
+                Configuration config = new Configuration(environment);
+                config.addMapper(AUserMapper.class);
+
+                SqlSessionFactoryBuilder builder = new SqlSessionFactoryBuilder();
+                fact = builder.build(config);
+        }
+
+        public static SqlSession
+                getSession() {
+                if (fact == null) {
+                        init();
+                }
+                return fact.openSession();
+        }
+}
---
+++ server/src/main/java/org/e8yes/srvs/dao/mappers/AUserMapper.java
@@ -0,0 +1,9 @@
+package org.e8yes.srvs.dao.mappers;
+
+/**
+ *
+ * @author daviswen
+ */
+public interface AUserMapper {
+        
+}
