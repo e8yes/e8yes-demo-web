ff5aa3be66c70db96943c5d84a28e8c9efcf101c
Chifeng Wen <daviesx66@gmail.com>
2020-03-17 21:06:35 -0700

    1. Adding file header. 2. Remove user initialization.

--- server/src/main/java/org/e8yes/environment/Initializer.java
+++ server/src/main/java/org/e8yes/environment/Initializer.java
@@ -1,7 +1,23 @@
+/**
+ * e8yes demo web server.
+ *
+ * <p>Copyright (C) 2020 Chifeng Wen {daviesx66@gmail.com}
+ *
+ * <p>This program is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * <p>This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * <p>You should have received a copy of the GNU General Public License along with this program. If
+ * not, see <http://www.gnu.org/licenses/>.
+ */
 package org.e8yes.environment;
 
 import org.e8yes.connection.DatabaseConnection;
-import org.e8yes.service.identity.User;
+import org.e8yes.service.identity.UserCreation;
 import org.e8yes.service.identity.UserGroup;
 
 /** Initialize the environment. */
@@ -10,6 +26,6 @@ public class Initializer {
   public static void init(EnvironmentContext ctx) {
     DatabaseConnection.init(ctx);
     UserGroup.init();
-    User.init(UserGroup.getContext());
+    UserCreation.init(UserGroup.getContext());
   }
 }
