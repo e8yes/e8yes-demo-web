90b8b52e38ef5d87b762a5f4431ff95698ee2fe9
Chifeng Wen <daviesx66@gmail.com>
2020-03-22 12:32:14 -0700

    Add equality and hash code functions to the Identity class.

--- server/src/main/java/org/e8yes/service/identity/Identity.java
+++ server/src/main/java/org/e8yes/service/identity/Identity.java
@@ -8,4 +8,24 @@ public class Identity {
   public Identity(long userId) {
     this.userId = userId;
   }
+
+  @Override
+  public int hashCode() {
+    return Long.hashCode(userId);
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj) {
+      return true;
+    }
+    if (obj == null) {
+      return false;
+    }
+    if (getClass() != obj.getClass()) {
+      return false;
+    }
+    final Identity other = (Identity) obj;
+    return this.userId == other.userId;
+  }
 }
