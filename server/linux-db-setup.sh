e46d521ff1d68c27e6715ea14c8ea5236bc7e79c
Chifeng Wen <daviesx66@gmail.com>
2020-04-08 01:09:29 +0100

    1. use pg command to start postgres services 2. change locale from en_US to en_GB

--- server/linux-db-setup.sh
+++ server/linux-db-setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo service postgresql start
+sudo pg_ctlcluster 11 main start
 echo "ALTER USER postgres WITH PASSWORD 'password';" | sudo -u postgres psql demoweb
 cat 'schema.pgsql' | sudo -u postgres psql demoweb
 cat 'schema.pgsql' | sudo -u postgres psql demowebtest
--- server/linux-pkgs-setup.sh
+++ server/linux-pkgs-setup.sh
@@ -3,4 +3,8 @@
 sudo apt install -y openjdk-11-jdk
 sudo apt install -y maven
 sudo apt install -y postgresql
+sudo apt install -y libprotobuf-dev
+sudo apt install -y libgrpc-dev
+sudo apt install -y protobuf-compiler
+sudo apt install -y protobuf-compiler-grpc
 
