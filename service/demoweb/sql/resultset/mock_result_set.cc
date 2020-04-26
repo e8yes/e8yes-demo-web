df0128881c84d3fae06fa56510bf44c123bf3d61
Chifeng Wen <daviesx66@gmail.com>
2020-04-26 14:02:18 -0700

    Add a mock implementation to the result set interface.

---
+++ service/demoweb/sql/resultset/mock_result_set.cc
@@ -0,0 +1,32 @@
+#include <cassert>
+#include <memory>
+
+#include "sql/reflection/sql_primitive_interface.h"
+#include "sql/resultset/mock_result_set.h"
+
+namespace e8 {
+
+MockResultSet::MockResultSet(unsigned num_cells) : num_cells_(num_cells) {}
+
+void MockResultSet::add_record(Record const &record) { records_.push_back(record); }
+
+void MockResultSet::next() {
+    assert(cur_record_ < records_.size());
+    ++cur_record_;
+}
+
+bool MockResultSet::has_next() const { return cur_record_ < records_.size(); }
+
+void MockResultSet::set_field(unsigned i, SqlPrimitiveInterface *field) {
+    assert(i < num_cells_);
+    Record const &record = records_[cur_record_];
+
+    std::shared_ptr<SqlPrimitiveInterface> cell = record[i];
+    if (cell == nullptr) {
+        return;
+    }
+
+    *field = *cell;
+}
+
+} // namespace e8
