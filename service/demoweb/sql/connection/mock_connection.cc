a441d5557265c3a1bea01ed46183979bf14cf8c0
Chifeng Wen <daviesx66@gmail.com>
2020-04-26 18:42:03 -0700

    Adds a mock connection that implements the connection interface.

---
+++ service/demoweb/sql/connection/mock_connection.cc
@@ -0,0 +1,96 @@
+/**
+ * e8yes demo web server.
+ *
+ * <p>Copyright (C) 2020 Chifeng Wen {daviesx66@gmail.com}
+ *
+ * <p>This program is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * <p>This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * <p>You should have received a copy of the GNU General Public License along with this program. If
+ * not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <algorithm>
+#include <cassert>
+#include <memory>
+#include <vector>
+
+#include "sql/connection/mock_connection.h"
+
+namespace e8 {
+
+MockConnection::MockQuerySetting::MockQuerySetting(ParameterizedQuery const &query,
+                                                   QueryParams const &params,
+                                                   MockResultSet const &result)
+    : query(query), params(params), result(result) {}
+
+bool MockConnection::MockQuerySetting::operator==(MockQuerySetting const &rhs) const {
+    return query == rhs.query && params.parameters() == rhs.params.parameters();
+}
+
+MockConnection::MockUpdateSetting::MockUpdateSetting(ParameterizedQuery const &query,
+                                                     QueryParams const &params,
+                                                     uint64_t num_rows_affected)
+    : query(query), params(params), num_rows_affected(num_rows_affected) {}
+
+bool MockConnection::MockUpdateSetting::operator==(MockUpdateSetting const &rhs) const {
+    return query == rhs.query && params.parameters() == rhs.params.parameters();
+}
+
+void MockConnection::set_query_result(ParameterizedQuery const &query, QueryParams const &params,
+                                      MockResultSet const &result) {
+    MockQuerySetting setting(query, params, result);
+
+    auto it = std::find(mock_query_results_.begin(), mock_query_results_.end(), setting);
+    if (it == mock_query_results_.end()) {
+        mock_query_results_.push_back(setting);
+    } else {
+        *it = setting;
+    }
+}
+
+void MockConnection::set_update_result(ParameterizedQuery const &query, QueryParams const &params,
+                                       uint64_t const &num_rows_affected) {
+    MockUpdateSetting setting(query, params, num_rows_affected);
+
+    auto it = std::find(mock_update_results_.begin(), mock_update_results_.end(), setting);
+    if (it == mock_update_results_.end()) {
+        mock_update_results_.push_back(setting);
+    } else {
+        *it = setting;
+    }
+}
+
+void MockConnection::set_closed(bool closed) { closed_ = closed; }
+
+std::unique_ptr<ResultSetInterface> MockConnection::run_query(ParameterizedQuery const &query,
+                                                              QueryParams const &params) {
+    auto it = std::find_if(mock_query_results_.begin(), mock_query_results_.end(),
+                           [&query, &params](MockQuerySetting const &setting) {
+                               return query == setting.query &&
+                                      params.parameters() == setting.params.parameters();
+                           });
+    assert(it != mock_query_results_.end());
+
+    return std::make_unique<MockResultSet>(it->result);
+}
+
+uint64_t MockConnection::run_update(ParameterizedQuery const &query, QueryParams const &params) {
+    auto it = std::find_if(mock_update_results_.begin(), mock_update_results_.end(),
+                           [&query, &params](MockUpdateSetting const &setting) {
+                               return query == setting.query &&
+                                      params.parameters() == setting.params.parameters();
+                           });
+    assert(it != mock_update_results_.end());
+
+    return it->num_rows_affected;
+}
+
+bool MockConnection::is_closed() const { return closed_; }
+
+} // namespace e8
---
+++ service/demoweb/sql/connection/mock_connection.h
@@ -0,0 +1,104 @@
+/**
+ * e8yes demo web server.
+ *
+ * <p>Copyright (C) 2020 Chifeng Wen {daviesx66@gmail.com}
+ *
+ * <p>This program is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * <p>This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * <p>You should have received a copy of the GNU General Public License along with this program. If
+ * not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef MOCK_CONNECTION_H
+#define MOCK_CONNECTION_H
+
+#include <cstdint>
+#include <memory>
+#include <vector>
+
+#include "sql/connection/connection_interface.h"
+#include "sql/resultset/mock_result_set.h"
+#include "sql/resultset/result_set_interface.h"
+
+namespace e8 {
+
+/**
+ * @brief The MockConnection class This implementations tailors to testing purposes.
+ */
+class MockConnection : public ConnectionInterface {
+  public:
+    MockConnection() = default;
+    MockConnection(MockConnection const &) = delete;
+    ~MockConnection() override = default;
+
+    /**
+     * @brief set_query_result Set the query result corresponding to the query and its parameteres.
+     * Repeated call of this function will override the previous setting. The program will fail, if
+     * the query result is not set before calling the below run_query() function.
+     */
+    void set_query_result(ParameterizedQuery const &query, QueryParams const &params,
+                          MockResultSet const &result);
+
+    /**
+     * @brief set_update_result Set the number of rows affected by the corresponding update query
+     * and its parameters. Repeated call of this function will override the previous setting.  The
+     * program will fail, if the query result is not set before calling the below run_update()
+     * function.
+     */
+    void set_update_result(ParameterizedQuery const &query, QueryParams const &params,
+                           uint64_t const &num_rows_affected);
+
+    /**
+     * @brief set_closed The the close state of this connection. Marking it closed will affect the
+     * result of is_closed().
+     * @param closed The new state of the this connection.
+     */
+    void set_closed(bool closed);
+
+    std::unique_ptr<ResultSetInterface> run_query(ParameterizedQuery const &query,
+                                                  QueryParams const &params) override;
+    uint64_t run_update(ParameterizedQuery const &query, QueryParams const &params) override;
+    bool is_closed() const override;
+
+  private:
+    struct MockQuerySetting {
+        MockQuerySetting(ParameterizedQuery const &query, QueryParams const &params,
+                         MockResultSet const &result);
+        MockQuerySetting(MockQuerySetting const &) = default;
+        ~MockQuerySetting() = default;
+
+        bool operator==(MockQuerySetting const &rhs) const;
+
+        ParameterizedQuery query;
+        QueryParams params;
+        MockResultSet result;
+    };
+
+    struct MockUpdateSetting {
+        MockUpdateSetting(ParameterizedQuery const &query, QueryParams const &params,
+                          uint64_t num_rows_affected);
+        MockUpdateSetting(MockUpdateSetting const &) = default;
+        ~MockUpdateSetting() = default;
+
+        bool operator==(MockUpdateSetting const &rhs) const;
+
+        ParameterizedQuery query;
+        QueryParams params;
+        uint64_t num_rows_affected;
+    };
+
+    std::vector<MockQuerySetting> mock_query_results_;
+    std::vector<MockUpdateSetting> mock_update_results_;
+    bool closed_ = false;
+    int8_t padding_[7];
+};
+
+} // namespace e8
+
+#endif // MOCK_CONNECTION_H
