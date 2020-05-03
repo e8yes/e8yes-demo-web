0596ab053666ab8bf0e86cc329f855456b9e7fc5
Chifeng Wen <daviesx66@gmail.com>
2020-05-03 14:59:27 -0700

    Refactor out a function to run a single bash command in a remote machine via ssh.

--- script/install_pkgs_for_node.sh
+++ script/install_pkgs_for_node.sh
@@ -20,7 +20,6 @@ fi
 
 echo "Installing kubernetes..."
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add - && \
-  echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list && \
-  sudo apt-get update -q && \
-  sudo apt-get install -qy kubeadm
+echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list && \
+sudo apt-get update -q && sudo apt-get install -qy kubeadm
 
