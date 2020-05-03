61a51fa7c99cbb700bed5dd4d9422eb2a1e87e42
Chifeng Wen <daviesx66@gmail.com>
2020-05-03 14:51:19 -0700

    init_node.sh doesn't actually complete all the initialization steps. Renaming it to install_pkgs_for_node.

--- init_nodes.py
+++ init_nodes.py
@@ -5,14 +5,14 @@ from script.parse_node_config import ClusterConfig
 from script.parse_node_config import ReadNodeConfig
 from script.run_bash_script import RunScriptInNode
 
-def InitNode(node_config: NodeConfig, init_script_file_path: str):
-  RunScriptInNode(node=node_config, script_file_path=init_script_file_path)
+def InstallPackagesForNode(node_config: NodeConfig, script_file_path: str):
+  RunScriptInNode(node=node_config, script_file_path=script_file_path)
 
 if __name__ == "__main__":
   node_configs, cluster_config = ReadNodeConfig(
     config_file_path="nodes_configuration.json")
   for node_name, node_config in node_configs.items():
-    print("Initializing node", node_name, node_config)
-    InitNode(node_config=node_config, 
-             init_script_file_path="script/init_node.sh")
+    print("Installing packages for node", node_name, node_config)
+    InstallPackagesForNode(node_config=node_config, 
+                           script_file_path="script/install_pkgs_for_node.sh")
 
