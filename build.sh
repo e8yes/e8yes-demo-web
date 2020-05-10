5b3da7de3d5ff7b2494ff5e81ef9c6e5e722d47c
Chifeng Wen <daviesx66@gmail.com>
2020-05-10 01:26:07 -0700

    Change the script to build all the images.

--- build.sh
+++ build.sh
@@ -2,4 +2,5 @@
 
 sudo docker pull ubuntu:20.04
 sudo docker build -t="demoweb_dev_env" .
+sudo docker build -t="demowebservice" demoweb_service
 
--- demoweb_service/build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-sudo docker build -t="demowebservice" .
-
