1110c9c18106777822af927c12b0f5d9908681d1
Chifeng Wen <daviesx66@gmail.com>
2020-05-10 01:31:58 -0700

    Move the build script into the script folder.

--- build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-sudo docker pull ubuntu:20.04
-sudo docker build -t="demoweb_dev_env" .
-sudo docker build -t="demowebservice" demoweb_service
-
--- script/build.sh
+++ script/build.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 
-set -e
-
-echo "Building shared images..."
-cd ../shared_image
-./build.sh
-cd ../script
-
-echo "Building demoweb service image..."
-cd ../demoweb_service
-./build.sh
-cd ../script
+sudo docker pull ubuntu:20.04
+sudo docker build -t="demoweb_dev_env" ..
+sudo docker build -t="demowebservice" ../demoweb_service
 
