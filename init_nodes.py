a44f1b13c14c7bcd81eb3ea7b40c05f5ee5ffe5f
Chifeng Wen <daviesx66@gmail.com>
2020-05-10 14:12:57 -0700

    Add script to refresh host keys before initializing nodes.

--- init_nodes.py
+++ init_nodes.py
@@ -6,6 +6,7 @@ from script.parse_node_config import ReadNodeConfig
 from script.run_bash_script import RunScriptInNode
 from script.run_bash_script import RunSingleCommandInNode
 from script.run_bash_script import ToSingleLineString
+from script.refresh_host_keys import RefreshHostKeys
 
 def InstallPackagesForNode(node_config: NodeConfig, script_file_path: str):
   RunScriptInNode(node=node_config, script_file_path=script_file_path)
@@ -55,6 +56,10 @@ def SetUpKubernetesMasterNodeCni(master_node_config: NodeConfig):
 if __name__ == "__main__":
   node_configs, cluster_config = ReadNodeConfig(
     config_file_path="nodes_configuration.json")
+
+  print("Refreshing host keys...")
+  RefreshHostKeys(node_configs.values())
+
   for node_name, node_config in node_configs.items():
     print("Installing packages for node", node_name, node_config)
     InstallPackagesForNode(node_config=node_config, 
---
+++ script/refresh_host_keys.py
@@ -0,0 +1,16 @@
+import os
+import subprocess
+from pathlib import Path
+from typing import List
+from script.parse_node_config import NodeConfig
+
+def RefreshHostKeys(nodes: List[NodeConfig]):
+  known_hosts_path = str(Path.home()) + "/.ssh/known_hosts"
+
+  if os.path.exists(known_hosts_path):
+    os.remove(known_hosts_path)
+  for node in nodes:
+    host_key = subprocess.check_output(["ssh-keyscan", node.location])[:-1]\
+      .decode("utf-8")
+    with open(known_hosts_path, "a") as known_hosts:
+      known_hosts.write(host_key)
