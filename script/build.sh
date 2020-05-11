7b51eaa5357f958907e575f868f933c209dae613
Chifeng Wen <daviesx66@gmail.com>
2020-05-11 02:23:19 -0700

    Accept an extra parameter to tag image with host name.

--- script/build.sh
+++ script/build.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-sudo docker pull ubuntu:19.10
+host=""
+if [ "$#" -eq 1 ]; then
+	host="$1:5000/"
+fi
+
 sudo docker build -t="demoweb_dev_env" ..
-sudo docker build -t="demowebservice" ../demoweb_service
+sudo docker build -t=$host"demowebservice" ../demoweb_service
 
