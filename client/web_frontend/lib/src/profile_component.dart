5ffa6c925e7ff9fd69f964d452ac4892bbffdc75
Chifeng Wen <daviesx66@gmail.com>
2020-06-10 12:14:17 -0700

    Move profile view to its the profile child component.

--- client/web_frontend/lib/src/profile_component.dart
+++ client/web_frontend/lib/src/profile_component.dart
@@ -1,8 +1,16 @@
 import 'package:angular/angular.dart';
+import 'package:demoweb_app/src/proto_dart/user_profile.pb.dart';
+import 'package:demoweb_app/src/user_service_interface.dart';
 
 @Component(
   selector: 'profile',
   templateUrl: 'profile_component.html',
+  directives: [coreDirectives],
 )
 class ProfileComponent {
+  @Input() UserPublicProfile profile;
+
+  final UserServiceInterface user_service_;
+
+  ProfileComponent(this.user_service_);
 }
