ea753ecb9e923168e7de94bf04806846ecce14d8
Chifeng Wen <daviesx66@gmail.com>
2020-06-11 08:32:21 -0700

    Should install go-dep.

--- third_party/grpcwebproxy/install_dev_pkgs.sh
+++ third_party/grpcwebproxy/install_dev_pkgs.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 apt install -y golang-go
+apt install -y go-dep
 
 GOPATH=~/go ; export GOPATH
 git clone https://github.com/improbable-eng/grpc-web.git $GOPATH/src/github.com/improbable-eng/grpc-web
-cd $GOPATH/src/github.com/improbable-eng/grpc-web
+pushd $GOPATH/src/github.com/improbable-eng/grpc-web
 dep ensure # after installing dep
 go install ./go/grpcwebproxy # installs into $GOPATH/bin/grpcwebproxy
+popd
 
