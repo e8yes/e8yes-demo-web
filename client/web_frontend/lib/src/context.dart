623888fec7c73cfaeca8065e17f89c87051f282b
Chifeng Wen <daviesx66@gmail.com>
2020-06-14 18:13:13 -0700

    Point back to the production service.

--- client/web_frontend/lib/src/context.dart
+++ client/web_frontend/lib/src/context.dart
@@ -3,8 +3,8 @@ import 'package:demoweb_app/src/credential_storage.dart';
 import 'package:demoweb_app/src/identity_storage.dart';
 import 'package:demoweb_app/src/immediate_connection.dart';
 
-// Uri demowebServiceUri = Uri.parse("http://api.e8yes.org:8000");
-Uri demowebServiceUri = Uri.parse("http://localhost:8000");
+Uri demowebServiceUri = Uri.parse("http://api.e8yes.org:8000");
+// Uri demowebServiceUri = Uri.parse("http://localhost:8000");
 
 ConnectionManagerInterface demowebServiceConnections =
     ImmediateConnection(demowebServiceUri);
