79970bfc884ae85a0d1c6dbc4df649b138022514
Chifeng Wen <daviesx66@gmail.com>
2020-06-21 20:12:02 -0700

    Override postgres configuration files.

--- postgres/install_dev_pkgs.sh
+++ postgres/install_dev_pkgs.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-echo "Installing postgres database engine and citus data extension..."
-curl https://install.citusdata.com/community/deb.sh | bash
-apt install -y postgresql-12-citus-9.3
+echo "Installing postgres database engine..."
+apt install -y postgresql
+
+echo "Copying config files..."
+cp ./conf/11/pg_hba.conf /etc/postgresql/11/main/pg_hba.conf
+cp ./conf/11/postgresql.conf /etc/postgresql/11/main/postgresql.conf
 
 echo "Launching postgres cluster..."
-systemctl start postgresql
+systemctl restart postgresql
 
 echo "Installing posgres C++ client library..."
 apt install -y libpqxx-dev
