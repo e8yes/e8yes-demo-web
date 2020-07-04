d7b16e1d64ecd893db7abcf47509324289240975
Chifeng Wen <daviesx66@gmail.com>
2020-07-03 23:50:02 -0700

    Display join date.

--- client/web_frontend/lib/src/profile_component.dart
+++ client/web_frontend/lib/src/profile_component.dart
@@ -1,11 +1,11 @@
 import 'package:angular/angular.dart';
 import 'package:angular_forms/angular_forms.dart';
 import 'package:demoweb_app/src/context.dart';
-
 import 'package:demoweb_app/src/proto_dart/nullable_primitives.pb.dart';
 import 'package:demoweb_app/src/proto_dart/user_profile.pb.dart';
 import 'package:demoweb_app/src/proto_dart/service_user.pb.dart';
 import 'package:demoweb_app/src/user_service_interface.dart';
+import 'package:fixnum/fixnum.dart';
 
 @Component(
   selector: 'profile',
@@ -28,6 +28,12 @@ class ProfileComponent {
     return profile.hasAlias() && profile.alias.value.isNotEmpty;
   }
 
+  String timestampToDateString(Int64 timestamp) {
+    return DateTime.fromMillisecondsSinceEpoch(timestamp.toInt())
+        .toLocal()
+        .toString();
+  }
+
   void onClickEdit() {
     editMode = true;
   }
@@ -38,7 +44,7 @@ class ProfileComponent {
       profile.alias = (NullableString()..value = newAlias);
     }
     req.alias = profile.alias;
-    
+
     String signature = credentialStorage.loadSignature();
 
     _user_service
