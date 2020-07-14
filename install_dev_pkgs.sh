d34c29351ad8b8618d67fd6aa6e7a59441ed623f
Chifeng Wen <daviesx66@gmail.com>
2020-07-14 11:04:14 -0700

    Add dependency zeromq.

--- install_dev_pkgs.sh
+++ install_dev_pkgs.sh
@@ -5,6 +5,6 @@ cd demoweb_service && ./install_dev_pkgs.sh && cd ..
 cd keygen && ./install_dev_pkgs.sh && cd ..
 cd client/web_frontend && ./install_dev_pkgs.sh && cd ../..
 cd third_party/grpcwebproxy && ./install_dev_pkgs.sh && cd ../..
-
 cd postgres && ./push_schema.sh && cd ..
+cd message_queue && ./install_dev_pkgs.sh && cd ../..
 
