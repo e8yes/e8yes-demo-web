edcb11837b024e22dca50741de4cd08cfd699a25
Chifeng Wen <daviesx66@gmail.com>
2020-07-14 23:52:39 -0700

    Add a class to map the database table message_channel to a C++ class.

---
+++ demoweb_service/demoweb/common_entity/message_channel_entity.cc
@@ -0,0 +1,41 @@
+/**
+ * e8yes demo web server.
+ *
+ * <p>Copyright (C) 2020 Chifeng Wen {daviesx66@gmail.com}
+ *
+ * <p>This program is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * <p>This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * <p>You should have received a copy of the GNU General Public License along with this program. If
+ * not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "demoweb_service/demoweb/common_entity/message_channel_entity.h"
+#include "postgres/query_runner/reflection/sql_entity_interface.h"
+#include "postgres/query_runner/reflection/sql_primitives.h"
+
+namespace e8 {
+
+MessageChannelEntity::MessageChannelEntity()
+    : SqlEntityInterface({&id, &channel_name, &created_at}) {}
+
+MessageChannelEntity::MessageChannelEntity(MessageChannelEntity const &other)
+    : MessageChannelEntity() {
+    id = other.id;
+    channel_name = other.channel_name;
+    created_at = other.created_at;
+}
+
+MessageChannelEntity &MessageChannelEntity::operator=(MessageChannelEntity const &other) {
+    id = other.id;
+    channel_name = other.channel_name;
+    created_at = other.created_at;
+    return *this;
+}
+
+} // namespace e8
---
+++ demoweb_service/demoweb/common_entity/message_channel_entity.h
@@ -0,0 +1,49 @@
+/**
+ * e8yes demo web server.
+ *
+ * <p>Copyright (C) 2020 Chifeng Wen {daviesx66@gmail.com}
+ *
+ * <p>This program is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * <p>This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * <p>You should have received a copy of the GNU General Public License along with this program. If
+ * not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef MESSAGECHANNELENTITY_H
+#define MESSAGECHANNELENTITY_H
+
+#include <cstdint>
+
+#include "postgres/query_runner/reflection/sql_entity_interface.h"
+#include "postgres/query_runner/reflection/sql_primitives.h"
+
+namespace e8 {
+
+using MessagechannelId = int64_t;
+
+/**
+ * @brief The MessageChannelEntity class C++ class representation of the database table
+ * "message_channel".
+ */
+class MessageChannelEntity : public SqlEntityInterface {
+  public:
+    MessageChannelEntity();
+    MessageChannelEntity(MessageChannelEntity const &other);
+    ~MessageChannelEntity() = default;
+
+    MessageChannelEntity &operator=(MessageChannelEntity const &other);
+
+    SqlLong id = SqlLong("id");
+    SqlStr channel_name = SqlStr("channel_name");
+    SqlTimestamp created_at = SqlTimestamp("created_at");
+};
+
+} // namespace e8
+
+#endif // MESSAGECHANNELENTITY_H
