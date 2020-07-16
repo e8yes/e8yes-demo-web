a618d1d4441b17ef0accb274d7d4193fbaddd42a
Chifeng Wen <daviesx66@gmail.com>
2020-07-15 17:42:51 -0700

    Add a class to map the database table message_channel_has_user.

---
+++ demoweb_service/demoweb/common_entity/message_channel_has_user_entity.h
@@ -0,0 +1,45 @@
+/**
+ * e8yes demo web server.
+ *
+ * <p>Copyright (C) 2020 Chifeng Wen {daviesx66@gmail.com}
+ *
+ * <p>This program is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * <p>This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * <p>You should have received a copy of the GNU General Public License along with this program. If
+ * not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef MESSAGECHANNELHASUSERENTITY_H
+#define MESSAGECHANNELHASUSERENTITY_H
+
+#include "postgres/query_runner/reflection/sql_entity_interface.h"
+#include "postgres/query_runner/reflection/sql_primitives.h"
+
+namespace e8 {
+
+/**
+ * @brief The MessageChannelHasUserEntity class C++ class representation of the database table
+ * "message_channel_has_user".
+ */
+class MessageChannelHasUserEntity : public SqlEntityInterface {
+  public:
+    MessageChannelHasUserEntity();
+    MessageChannelHasUserEntity(MessageChannelHasUserEntity const &other);
+    ~MessageChannelHasUserEntity() = default;
+
+    MessageChannelHasUserEntity &operator=(MessageChannelHasUserEntity const &other);
+
+    SqlLong channel_id = SqlLong("channel_id");
+    SqlLong user_id = SqlLong("user_id");
+    SqlInt ownership = SqlInt("ownership");
+};
+
+} // namespace e8
+
+#endif // MESSAGECHANNELHASUSERENTITY_H
