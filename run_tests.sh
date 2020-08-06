711fb11830ac1dee25796f4568196eb5cfe3e14b
Chifeng Wen <daviesx66@gmail.com>
2020-08-06 00:16:40 -0700

    Add the message queue path to the library export path.

--- run_tests.sh
+++ run_tests.sh
@@ -19,7 +19,7 @@ cd ..
 
 echo "============================================"
 echo "Adding the following DLLs to LD_LIBRARY_PATH"
-dlls=($(find . -regextype sed -regex ".*/build/\(proto_cc\|identity\|demoweb_service/demoweb\|postgres/query_runner\|common/container\|common/flags\|keygen\|third_party/base64\)"))
+dlls=($(find . -regextype sed -regex ".*/build/\(proto_cc\|identity\|demoweb_service/demoweb\|postgres/query_runner\|common/container\|common/flags\|keygen\|third_party/base64\|message_queue/message_queue\)"))
 for dll in ${dlls[@]}
 do
 	echo $dll
