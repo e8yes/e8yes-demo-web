981e984b84aa302eae208ce450c6d9a01cc81fb3
Chifeng Wen <daviesx66@gmail.com>
2020-08-25 12:54:38 -0700

    Add an installation script for the script module.

--- install_dev_pkgs.sh
+++ install_dev_pkgs.sh
@@ -9,6 +9,7 @@ sudo apt install -y protobuf-compiler
 echo "Installing Qt5 development packages..."
 sudo apt install -y qt5-default
 
+cd script && ./install_dev_pkgs.sh && cd ..
 cd postgres && ./install_dev_pkgs.sh && cd ..
 cd demoweb_service && ./install_dev_pkgs.sh && cd ..
 cd keygen && ./install_dev_pkgs.sh && cd ..
@@ -17,3 +18,4 @@ cd distributor && ./install_dev_pkgs.sh && cd ..
 cd client/web_frontend && ./install_dev_pkgs.sh && cd ../..
 cd third_party/grpcwebproxy && ./install_dev_pkgs.sh && cd ../..
 cd postgres && ./push_schema.sh && cd ..
+
---
+++ script/install_dev_pkgs.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Installing python gRPC and protobuf development packages..."
+sudo apt install -y python3-protobuf
+sudo apt install -y python3-grpcio
+sudo apt install -y python3-grpc-tools
+
