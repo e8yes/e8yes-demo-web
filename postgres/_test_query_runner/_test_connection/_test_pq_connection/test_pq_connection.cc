e9a5b75d30f39392f26c255f83ad6177acde90c8
Chifeng Wen <daviesx66@gmail.com>
2020-08-30 19:36:03 -0700

    Move pq_connection to the new testing framework.

--- postgres/_test_query_runner/_test_connection/_test_pq_connection/test_pq_connection.cc
+++ postgres/_test_query_runner/_test_connection/_test_pq_connection/test_pq_connection.cc
@@ -15,40 +15,26 @@
  * not, see <http://www.gnu.org/licenses/>.
  */
 
-#include <QtTest>
 #include <cstdint>
 #include <memory>
 #include <optional>
 #include <string>
 
+#include "common/unit_test_util/unit_test_util.h"
 #include "postgres/query_runner/connection/connection_interface.h"
 #include "postgres/query_runner/connection/pq_connection.h"
 #include "postgres/query_runner/reflection/sql_primitives.h"
 
-class pq_connection_test : public QObject {
-    Q_OBJECT
-
-  public:
-    pq_connection_test();
-    ~pq_connection_test();
-
-  private slots:
-    void connection_state_test();
-    void update_and_query_test();
-};
-
-pq_connection_test::pq_connection_test() {}
-
-pq_connection_test::~pq_connection_test() {}
-
-void pq_connection_test::connection_state_test() {
+bool ConnectionStateTest() {
     e8::PqConnection conn(
         /*host_name=*/"localhost",
         /*db_name=*/"demoweb");
-    QVERIFY(!conn.IsClosed());
+    TEST_CONDITION(!conn.IsClosed());
+
+    return true;
 }
 
-void pq_connection_test::update_and_query_test() {
+bool UpdateAndQueryTest() {
     e8::PqConnection conn(
         /*host_name=*/"localhost",
         /*db_name=*/"demoweb");
@@ -70,7 +56,7 @@ void pq_connection_test::update_and_query_test() {
     insertion_params.SetParam(2, std::make_shared<e8::SqlStr>("test_string", /*field_name=*/""));
     uint64_t num_rows_affected = conn.RunUpdate(insert_stmt, insertion_params);
 
-    QVERIFY(num_rows_affected == 1);
+    TEST_CONDITION(num_rows_affected == 1);
 
     // Query records.
     std::string query_stmt = "SELECT * FROM PqConnectionTest WHERE id=$1";
@@ -78,23 +64,29 @@ void pq_connection_test::update_and_query_test() {
     query_params.SetParam(1, std::make_shared<e8::SqlLong>(1L));
 
     std::unique_ptr<e8::ResultSetInterface> rs = conn.RunQuery(query_stmt, query_params);
-    QVERIFY(rs->HasNext());
+    TEST_CONDITION(rs->HasNext());
 
     e8::SqlLong id_field("id");
     e8::SqlStr test_name_field("test_name");
     rs->SetField(0, &id_field);
     rs->SetField(1, &test_name_field);
 
-    QVERIFY(id_field.Value() == std::optional<int64_t>(1L));
-    QVERIFY(test_name_field.Value() == std::optional<std::string>("test_string"));
+    TEST_CONDITION(id_field.Value() == std::optional<int64_t>(1L));
+    TEST_CONDITION(test_name_field.Value() == std::optional<std::string>("test_string"));
 
     rs->Next();
-    QVERIFY(!rs->HasNext());
+    TEST_CONDITION(!rs->HasNext());
 
     // Clean up.
     conn.RunUpdate(drop_table_stmt, e8::ConnectionInterface::QueryParams());
+
+    return true;
 }
 
-QTEST_APPLESS_MAIN(pq_connection_test)
-
-#include "tst_pq_connection_test.moc"
+int main() {
+    e8::BeginTestSuite("pq_connection");
+    e8::RunTest("ConnectionStateTest", ConnectionStateTest);
+    e8::RunTest("UpdateAndQueryTest", UpdateAndQueryTest);
+    e8::EndTestSuite();
+    return 0;
+}
--- postgres/postgres.pro
+++ postgres/postgres.pro
@@ -3,7 +3,7 @@ SUBDIRS = \
     query_runner/query_runner.pro \
     query_runner_test/orm/query_completion_test \
     query_runner_test/orm/data_collection_test \
-    query_runner_test/connection/pq_connection_test \
+    _test_query_runner/_test_connection/_test_pq_connection/_test_pq_connection.pro \
     _test_query_runner/_test_resultset/_test_pq_result_set/_test_pq_result_set.pro \
     _test_query_runner/_test_sql_runner/_test_sql_runner.pro \
     _test_query_runner/_test_sql_query_builder/_test_sql_query_builder.pro
