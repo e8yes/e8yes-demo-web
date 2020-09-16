e06b5b738e0d7a9f696550900d198f913a9f8125
Chifeng Wen <daviesx66@gmail.com>
2020-09-15 20:19:46 -0700

    Add a class to map to the message group table.

---
+++ demoweb_service/demoweb/common_entity/message_group_entity.cc
@@ -0,0 +1,49 @@
+/**
+ * e8yes demo web.
+ *
+ * <p>Copyright (C) 2020 Chifeng Wen {daviesx66@gmail.com}
+ *
+ * <p>This program is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * <p>This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * <p>You should have received a copy of the GNU General Public License along with this program. If
+ * not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "demoweb_service/demoweb/common_entity/message_group_entity.h"
+#include "postgres/query_runner/reflection/sql_entity_interface.h"
+#include "postgres/query_runner/reflection/sql_primitives.h"
+
+namespace e8 {
+
+MessageGroupEntity::MessageGroupEntity()
+    : SqlEntityInterface({&id, &group_type, &channel_id, &creator_id, &description, &created_at,
+                          &last_interaction_at}) {}
+
+MessageGroupEntity::MessageGroupEntity(MessageGroupEntity const &other) : MessageGroupEntity() {
+    id = other.id;
+    group_type = other.group_type;
+    channel_id = other.channel_id;
+    creator_id = other.creator_id;
+    description = other.description;
+    created_at = other.created_at;
+    last_interaction_at = other.last_interaction_at;
+}
+
+MessageGroupEntity &MessageGroupEntity::operator=(MessageGroupEntity const &other) {
+    id = other.id;
+    group_type = other.group_type;
+    channel_id = other.channel_id;
+    creator_id = other.creator_id;
+    description = other.description;
+    created_at = other.created_at;
+    last_interaction_at = other.last_interaction_at;
+    return *this;
+}
+
+} // namespace e8
---
+++ demoweb_service/demoweb/common_entity/message_group_entity.h
@@ -0,0 +1,49 @@
+/**
+ * e8yes demo web.
+ *
+ * <p>Copyright (C) 2020 Chifeng Wen {daviesx66@gmail.com}
+ *
+ * <p>This program is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * <p>This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * <p>You should have received a copy of the GNU General Public License along with this program. If
+ * not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef MESSAGE_GROUP_ENTITY_H
+#define MESSAGE_GROUP_ENTITY_H
+
+#include "postgres/query_runner/reflection/sql_entity_interface.h"
+#include "postgres/query_runner/reflection/sql_primitives.h"
+
+namespace e8 {
+
+/**
+ * @brief The MessageGroupEntity class C++ class representation of the database table
+ * "message_group".
+ */
+class MessageGroupEntity : public SqlEntityInterface {
+  public:
+    MessageGroupEntity();
+    MessageGroupEntity(MessageGroupEntity const &other);
+    ~MessageGroupEntity() = default;
+
+    MessageGroupEntity &operator=(MessageGroupEntity const &other);
+
+    SqlLong id = SqlLong("id");
+    SqlInt group_type = SqlInt("group_type");
+    SqlLong channel_id = SqlLong("channel_id");
+    SqlLong creator_id = SqlLong("creator_id");
+    SqlStr description = SqlStr("description");
+    SqlTimestamp created_at = SqlTimestamp("created_at");
+    SqlTimestamp last_interaction_at = SqlTimestamp("last_interaction_at");
+};
+
+} // namespace e8
+
+#endif // MESSAGE_GROUP_ENTITY_H
