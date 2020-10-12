e5c2dfe5744923c2a391e90443998718a26d5fe8
Chifeng Wen <daviesx66@gmail.com>
2020-10-12 00:13:14 -0700

    Add an interface and a default implementation defining access control policies surrounding the message channel resource.

--- demoweb_service/demoweb/pbac/message_channel_pbac.cc
+++ demoweb_service/demoweb/pbac/message_channel_pbac.cc
@@ -1,3 +1,129 @@
-#include "demoweb_service/demoweb/pbac/message_channel_pbac.h"
+/**
+ * e8yes demo web.
+ *
+ * <p>Copyright (C) 2020 Chifeng Wen {daviesx66@gmail.com}
+ *
+ * <p>This program is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * <p>This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * <p>You should have received a copy of the GNU General Public License along with this program. If
+ * not, see <http://www.gnu.org/licenses/>.
+ */
 
-MessageChannelPbac::MessageChannelPbac() {}
+#include <optional>
+#include <unordered_map>
+
+#include "demoweb_service/demoweb/common_entity/message_channel_entity.h"
+#include "demoweb_service/demoweb/common_entity/user_entity.h"
+#include "demoweb_service/demoweb/pbac/message_channel_attributes.h"
+#include "demoweb_service/demoweb/pbac/message_channel_member_attributes.h"
+#include "demoweb_service/demoweb/pbac/message_channel_pbac.h"
+#include "postgres/query_runner/connection/connection_reservoir_interface.h"
+#include "proto_cc/message_channel.pb.h"
+
+namespace e8 {
+
+MessageChannelPbacInterface::MessageChannelPbacInterface() {}
+
+MessageChannelPbacInterface::~MessageChannelPbacInterface() {}
+
+MessageChannelPbacImpl::MessageChannelPbacImpl(ConnectionReservoirInterface *conns)
+    : conns_(conns) {}
+
+MessageChannelPbacImpl::~MessageChannelPbacImpl() {}
+
+bool MessageChannelPbacImpl::AllowUpdateChannelMetadata(UserId const operator_user_id,
+                                                        MessageChannelId const target_channel_id) {
+    std::optional<MessageChannelAttributes> channel_attrs =
+        ExtractMessageChannelAttributes(target_channel_id, conns_);
+    if (!channel_attrs.has_value()) {
+        return false;
+    }
+
+    std::optional<MessageChannelMemberAttributes> operator_attrs =
+        ExtractMessageChannelMemberAttributes(target_channel_id, operator_user_id, conns_);
+    if (!operator_attrs.has_value()) {
+        return false;
+    }
+
+    if (*channel_attrs->close_group_channel.Value()) {
+        return true;
+    }
+
+    return operator_attrs->member_type == MCMT_ADMIN;
+}
+
+bool MessageChannelPbacImpl::AllowDeleteChannel(UserId const operator_user_id,
+                                                MessageChannelId const target_channel_id) {
+    return this->AllowUpdateChannelMetadata(operator_user_id, target_channel_id);
+}
+
+bool MessageChannelPbacImpl::AllowUpdateChannelMembership(UserId const operator_user_id,
+                                                          MessageChannelId const target_channel_id,
+                                                          UserId const /*user_to_be_updated*/,
+                                                          MessageChannelMemberType member_type) {
+    std::optional<MessageChannelAttributes> channel_attrs =
+        ExtractMessageChannelAttributes(target_channel_id, conns_);
+    if (!channel_attrs.has_value()) {
+        return false;
+    }
+
+    std::optional<MessageChannelMemberAttributes> operator_attrs =
+        ExtractMessageChannelMemberAttributes(target_channel_id, operator_user_id, conns_);
+    if (!operator_attrs.has_value()) {
+        return false;
+    }
+
+    switch (member_type) {
+    case MCMT_ADMIN:
+        return operator_attrs->member_type == MCMT_ADMIN;
+
+    case MCMT_MEMBER:
+        if (*channel_attrs->close_group_channel.Value()) {
+            return true;
+        }
+        return operator_attrs->member_type == MCMT_ADMIN;
+
+    case MCMT_INVALID:
+    default:
+        return false;
+    }
+}
+
+bool MessageChannelPbacImpl::AllowDeleteMemberFromChannel(UserId const operator_user_id,
+                                                          MessageChannelId const target_channel_id,
+                                                          UserId const user_to_be_removed) {
+    std::optional<MessageChannelAttributes> channel_attrs =
+        ExtractMessageChannelAttributes(target_channel_id, conns_);
+    if (!channel_attrs.has_value()) {
+        return false;
+    }
+
+    std::unordered_map<UserId, MessageChannelMemberAttributes> member_attrs =
+        ExtractMessageChannelMemberAttributes(target_channel_id, conns_);
+    auto operator_attr_it = member_attrs.find(operator_user_id);
+    if (operator_attr_it == member_attrs.end()) {
+        return false;
+    }
+
+    auto to_be_removed_attr_it = member_attrs.find(user_to_be_removed);
+    if (to_be_removed_attr_it == member_attrs.end()) {
+        return false;
+    }
+
+    if (operator_attr_it->second.member_type != MCMT_ADMIN) {
+        return false;
+    }
+    if (operator_user_id == user_to_be_removed && member_attrs.size() == 1) {
+        return false;
+    }
+
+    return true;
+}
+
+} // namespace e8
--- demoweb_service/demoweb/pbac/message_channel_pbac.h
+++ demoweb_service/demoweb/pbac/message_channel_pbac.h
@@ -1,11 +1,100 @@
 #ifndef MESSAGECHANNELPBAC_H
 #define MESSAGECHANNELPBAC_H
 
+#include "demoweb_service/demoweb/common_entity/message_channel_entity.h"
+#include "demoweb_service/demoweb/common_entity/user_entity.h"
+#include "postgres/query_runner/connection/connection_reservoir_interface.h"
+#include "proto_cc/message_channel.pb.h"
 
-class MessageChannelPbac
-{
-public:
-    MessageChannelPbac();
+namespace e8 {
+
+/**
+ * @brief The MessageChannelPbacInterface class Defines access control policies surrounding the
+ * message channel resource.
+ */
+class MessageChannelPbacInterface {
+  public:
+    MessageChannelPbacInterface();
+    virtual ~MessageChannelPbacInterface();
+
+    /**
+     * @brief AllowUpdateChannelMetadata Check if the operator has the permission to change the
+     * metadata of the specified message channel.
+     *
+     * @param operator_user_id ID of the person who is requesting the update.
+     * @param target_channel_id ID of the message channel to be updated.
+     * @return Whether the operator has the right.
+     */
+    virtual bool AllowUpdateChannelMetadata(UserId const operator_user_id,
+                                            MessageChannelId const target_channel_id) = 0;
+
+    /**
+     * @brief AllowDeleteChannel Check if the operator has the permission to delete the specified
+     * message channel.
+     *
+     * @param operator_user_id ID of the person who is requesting the deletion.
+     * @param target_channel_id ID of the message channel to be deleted.
+     * @return Whether the operator has the right.
+     */
+    virtual bool AllowDeleteChannel(UserId const operator_user_id,
+                                    MessageChannelId const target_channel_id) = 0;
+
+    /**
+     * @brief AllowUpdateChannelMembership Check if the operator has the permission to add or change
+     * the membership to the specified message channel.
+     *
+     * @param operator_user_id ID of the person who is requesting the change to the membership.
+     * @param target_channel_id ID of the message channel to be changed membership from.
+     * @param user_to_be_updated ID of the person to be update the channel membership of.
+     * @param member_type The new member type the operator is requesting to update to.
+     * @return Whether the operator has the right.
+     */
+    virtual bool AllowUpdateChannelMembership(UserId const operator_user_id,
+                                              MessageChannelId const target_channel_id,
+                                              UserId const user_to_be_updated,
+                                              MessageChannelMemberType member_type) = 0;
+
+    /**
+     * @brief AllowDeleteMemberFromChannel Check if the operator has the permission to remove the
+     * specified member from the message channel.
+     *
+     * @param operator_user_id ID of the person who is requesting the member's removal.
+     * @param target_channel_id ID of the message channel to be updated with removal.
+     * @param user_to_be_removed ID of the channel member to be removed.
+     * @return Whether the operator has the right.
+     */
+    virtual bool AllowDeleteMemberFromChannel(UserId const operator_user_id,
+                                              MessageChannelId const target_channel_id,
+                                              UserId const user_to_be_removed) = 0;
 };
+
+/**
+ * @brief The MessageChannelPbacImpl class Default implementation of the access control policy.
+ */
+class MessageChannelPbacImpl : public MessageChannelPbacInterface {
+  public:
+    MessageChannelPbacImpl(ConnectionReservoirInterface *conns);
+    ~MessageChannelPbacImpl() override;
+
+    bool AllowUpdateChannelMetadata(UserId const operator_user_id,
+                                    MessageChannelId const target_channel_id) override;
+
+    bool AllowDeleteChannel(UserId const operator_user_id,
+                            MessageChannelId const target_channel_id) override;
+
+    bool AllowUpdateChannelMembership(UserId const operator_user_id,
+                                      MessageChannelId const target_channel_id,
+                                      UserId const user_to_be_added,
+                                      MessageChannelMemberType member_type) override;
+
+    bool AllowDeleteMemberFromChannel(UserId const operator_user_id,
+                                      MessageChannelId const target_channel_id,
+                                      UserId const user_to_be_removed) override;
+
+  private:
+    ConnectionReservoirInterface *conns_;
+};
+
+} // namespace e8
 
 #endif // MESSAGECHANNELPBAC_H
