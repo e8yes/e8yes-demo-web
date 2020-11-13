44bbab07a581a960ce5c7d9784b44afd47d0c31c
Chifeng Wen <daviesx66@gmail.com>
2020-11-12 17:53:04 -0800

    Change port numbers to 1-to-1 mapped to the external ones.

--- client/web_frontend/lib/src/context.dart
+++ client/web_frontend/lib/src/context.dart
@@ -4,7 +4,7 @@ import 'package:demoweb_app/src/grpc_requester.dart';
 import 'package:demoweb_app/src/identity_storage.dart';
 import 'package:demoweb_app/src/immediate_connection.dart';
 
-Uri demowebServiceUri = Uri.parse("http://api.e8yes.org:18000");
+Uri demowebServiceUri = Uri.parse("http://api.e8yes.org:8000");
 // Uri demowebServiceUri = Uri.parse("http://localhost:8000");
 
 ConnectionManagerInterface demowebServiceConnections =
