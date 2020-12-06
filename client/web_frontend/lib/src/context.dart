cbb7a2e89d56471555a46413dc5330644c40526d
Chifeng Wen <daviesx66@gmail.com>
2020-12-06 06:52:14 -0800

    Subdomain api.e8yes.org is bandoned.

--- client/web_frontend/lib/src/context.dart
+++ client/web_frontend/lib/src/context.dart
@@ -4,7 +4,7 @@ import 'package:demoweb_app/src/grpc_requester.dart';
 import 'package:demoweb_app/src/identity_storage.dart';
 import 'package:demoweb_app/src/immediate_connection.dart';
 
-Uri demowebServiceUri = Uri.parse("http://api.e8yes.org:18000");
+Uri demowebServiceUri = Uri.parse("http://home.e8yes.org:18000");
 // Uri demowebServiceUri = Uri.parse("http://192.168.30.202");
 
 ConnectionManagerInterface demowebServiceConnections =
