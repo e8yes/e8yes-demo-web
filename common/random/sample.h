5eea8474b3797e9970eeeb52a9bdc82c80dc3f7b
Chifeng Wen <daviesx66@gmail.com>
2021-01-19 07:07:31 -0800

    Add a module to sample outcomes from a given discrete distribution.

---
+++ common/random/sample.h
@@ -0,0 +1,49 @@
+/**
+ * e8yes demo web.
+ *
+ * <p>Copyright (C) 2020 Chifeng Wen {daviesx66@gmail.com}
+ *
+ * <p>This program is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * <p>This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * <p>You should have received a copy of the GNU General Public License along with this program. If
+ * not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef SAMPLE_H
+#define SAMPLE_H
+
+#include <cassert>
+#include <unordered_map>
+#include <vector>
+
+#include "common/random/random_source.h"
+
+namespace e8 {
+
+/**
+ * @brief SampleFrom Sample a key from the specified discrete distribution with the key's
+ * probability.
+ */
+template <typename KeyType>
+KeyType SampleFrom(std::unordered_map<KeyType, float> const &discrete_distri,
+                   RandomSource *random_source) {
+    double q = random_source->Draw();
+    double cdf = 0;
+    for (auto const &[key, p] : discrete_distri) {
+        cdf += p;
+        if (q < cdf) {
+            return key;
+        }
+    }
+    assert(false);
+}
+
+} // namespace e8
+
+#endif // SAMPLE_H
