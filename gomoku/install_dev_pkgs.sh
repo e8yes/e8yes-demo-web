c854434d42bb7d774b518d79a6389e99975b8c96
Chifeng Wen <daviesx66@gmail.com>
2021-01-26 02:12:23 -0800

    Update build and dev dependencies.

--- gomoku/install_dev_pkgs.sh
+++ gomoku/install_dev_pkgs.sh
@@ -6,7 +6,9 @@ sudo apt install -y libprotobuf-dev
 echo "Installing python numpy packages..."
 sudo apt install -y python3-numpy
 
+echo "Installing python psycopg2 packages..."
+sudo apt install -y python3-psycopg2
+
 echo "Installing python tensorflow packages..."
 sudo pip3 install --upgrade tensorflow
-
 
