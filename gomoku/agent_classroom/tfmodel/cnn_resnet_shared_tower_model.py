2e8277ac914b9484d693088de7b0d0c49f748731
Chifeng Wen <daviesx66@gmail.com>
2021-01-29 02:40:37 -0800

    Assign names to import ops.

--- gomoku/agent_classroom/tfmodel/cnn_resnet_shared_tower_model.py
+++ gomoku/agent_classroom/tfmodel/cnn_resnet_shared_tower_model.py
@@ -191,55 +191,77 @@ class GomokuCnnResNetSharedTower(tf.Module):
                  game_phase_standard_gomoku: np.ndarray,
                  next_move_stone_type) -> Tuple[tf.Tensor, tf.Tensor, tf.Tensor]:
         # Places the game states into feature planes.
-        black_stones = -tf.minimum(x=board_features, y=0.0) # pylint: disable=invalid-unary-operand-type
+        black_stones = -tf.minimum(x=board_features, y=0.0, # pylint: disable=invalid-unary-operand-type
+                                   name="black_stones_extraction")
         black_stones = tf.reshape(tensor=black_stones,
-                                  shape=[-1, INPUT_SIZE, INPUT_SIZE, 1])
+                                  shape=[-1, INPUT_SIZE, INPUT_SIZE, 1],
+                                  name="black_stones_create_channel")
 
-        white_stones = tf.maximum(x=board_features, y=0.0)
+        white_stones = tf.maximum(x=board_features, y=0.0,
+                                  name="white_stones_extraction")
         white_stones = tf.reshape(tensor=white_stones,
-                                  shape=[-1, INPUT_SIZE, INPUT_SIZE, 1])
+                                  shape=[-1, INPUT_SIZE, INPUT_SIZE, 1],
+                                  name="white_stones_create_channel")
 
         game_phase_place_3_stones = \
-            tf.reshape(tensor=game_phase_place_3_stones, shape=[-1, 1, 1, 1])
+            tf.reshape(tensor=game_phase_place_3_stones, shape=[-1, 1, 1, 1],
+                       name="game_phase_place_3_stones_dim3")
         game_phase_place_3_stones = \
-            tf.repeat(input=game_phase_place_3_stones, repeats=[INPUT_SIZE], axis=2)
+            tf.repeat(input=game_phase_place_3_stones, repeats=[INPUT_SIZE], axis=2,
+                      name="game_phase_place_3_stones_dim2")
         game_phase_place_3_stones = \
-            tf.repeat(input=game_phase_place_3_stones, repeats=[INPUT_SIZE], axis=1)
+            tf.repeat(input=game_phase_place_3_stones, repeats=[INPUT_SIZE], axis=1,
+                      name="game_phase_place_3_stones_dim1")
 
         game_phase_swap2_decision = \
-            tf.reshape(tensor=game_phase_swap2_decision, shape=[-1, 1, 1, 1])
+            tf.reshape(tensor=game_phase_swap2_decision, shape=[-1, 1, 1, 1],
+                       name="game_phase_swap2_decision_dim3")
         game_phase_swap2_decision = \
-            tf.repeat(input=game_phase_swap2_decision, repeats=[INPUT_SIZE], axis=2)
+            tf.repeat(input=game_phase_swap2_decision, repeats=[INPUT_SIZE], axis=2,
+                      name="game_phase_swap2_decision_dim2")
         game_phase_swap2_decision = \
-            tf.repeat(input=game_phase_swap2_decision, repeats=[INPUT_SIZE], axis=1)
+            tf.repeat(input=game_phase_swap2_decision, repeats=[INPUT_SIZE], axis=1,
+                      name="game_phase_swap2_decision_dim1")
 
         game_phase_place2_more_stones = \
-            tf.reshape(tensor=game_phase_place2_more_stones, shape=[-1, 1, 1, 1])
+            tf.reshape(tensor=game_phase_place2_more_stones, shape=[-1, 1, 1, 1],
+                       name="game_phase_place2_more_stones_dim3")
         game_phase_place2_more_stones = \
-            tf.repeat(input=game_phase_place2_more_stones, repeats=[INPUT_SIZE], axis=2)
+            tf.repeat(input=game_phase_place2_more_stones, repeats=[INPUT_SIZE], axis=2,
+                      name="game_phase_place2_more_stones_dim2")
         game_phase_place2_more_stones = \
-            tf.repeat(input=game_phase_place2_more_stones, repeats=[INPUT_SIZE], axis=1)
+            tf.repeat(input=game_phase_place2_more_stones, repeats=[INPUT_SIZE], axis=1,
+                      name="game_phase_place2_more_stones_dim1")
 
         game_phase_stone_type_decision = \
-            tf.reshape(tensor=game_phase_stone_type_decision, shape=[-1, 1, 1, 1])
+            tf.reshape(tensor=game_phase_stone_type_decision, shape=[-1, 1, 1, 1],
+                       name="game_phase_stone_type_decision_dim3")
         game_phase_stone_type_decision = \
-            tf.repeat(input=game_phase_stone_type_decision, repeats=[INPUT_SIZE], axis=2)
+            tf.repeat(input=game_phase_stone_type_decision, repeats=[INPUT_SIZE], axis=2,
+                      name="game_phase_stone_type_decision_dim2")
         game_phase_stone_type_decision = \
-            tf.repeat(input=game_phase_stone_type_decision, repeats=[INPUT_SIZE], axis=1)
+            tf.repeat(input=game_phase_stone_type_decision, repeats=[INPUT_SIZE], axis=1,
+                      name="game_phase_stone_type_decision_dim1")
 
         game_phase_standard_gomoku = \
-            tf.reshape(tensor=game_phase_standard_gomoku, shape=[-1, 1, 1, 1])
+            tf.reshape(tensor=game_phase_standard_gomoku, shape=[-1, 1, 1, 1],
+                       name="game_phase_standard_gomoku_dim3")
         game_phase_standard_gomoku = \
-            tf.repeat(input=game_phase_standard_gomoku, repeats=[INPUT_SIZE], axis=2)
+            tf.repeat(input=game_phase_standard_gomoku, repeats=[INPUT_SIZE], axis=2,
+                      name="game_phase_standard_gomoku_dim2")
         game_phase_standard_gomoku = \
-            tf.repeat(input=game_phase_standard_gomoku, repeats=[INPUT_SIZE], axis=1)
+            tf.repeat(input=game_phase_standard_gomoku, repeats=[INPUT_SIZE], axis=1,
+                      name="game_phase_standard_gomoku_dim1")
 
         next_move_stone_type = \
-            tf.reshape(tensor=next_move_stone_type, shape=[-1, 1, 1, 1])
+            tf.reshape(tensor=next_move_stone_type, shape=[-1, 1, 1, 1],
+                      name="next_move_stone_type_dim3")
         next_move_stone_type = \
-            tf.repeat(input=next_move_stone_type, repeats=[INPUT_SIZE], axis=2)
+            tf.repeat(input=next_move_stone_type, repeats=[INPUT_SIZE], axis=2,
+                      name="next_move_stone_type_dim2")
         next_move_stone_type = \
-            tf.repeat(input=next_move_stone_type, repeats=[INPUT_SIZE], axis=1)
+            tf.repeat(input=next_move_stone_type, repeats=[INPUT_SIZE], axis=1,
+                      name="next_move_stone_type_dim1")
         
         conv_features = tf.concat(values=[black_stones, # pylint: disable=unexpected-keyword-arg, no-value-for-parameter
                                           white_stones,
@@ -249,67 +271,92 @@ class GomokuCnnResNetSharedTower(tf.Module):
                                           game_phase_stone_type_decision,
                                           game_phase_standard_gomoku,
                                           next_move_stone_type],
-                                  axis=3)
+                                  axis=3,
+                                  name="input_feature_planes")
 
         # Residual tower head
-        conv_features = tf.nn.conv2d(input=conv_features,
-                                     filters=self.res_tower_head_layer_.conv_kernel,
-                                     strides=[1, 1, 1, 1],
-                                     padding="SAME") + \
-                        self.res_tower_head_layer_.biases
-        conv_features = tf.nn.leaky_relu(features=conv_features)
+        tower_head_linear = tf.nn.conv2d(input=conv_features,
+                                         filters=self.res_tower_head_layer_.conv_kernel,
+                                         strides=[1, 1, 1, 1],
+                                         padding="SAME",
+                                         name="residual_tower_head_conv") + \
+                            self.res_tower_head_layer_.biases
+        tower_head = tf.nn.leaky_relu(features=tower_head_linear,
+                                      name="residual_tower_head")
 
         # Constructs the residual tower.
+        tower_hidden_outputs = [tower_head]
         for res_block in self.res_blocks_:
-            conv_output = tf.nn.conv2d(input=conv_features,
-                                       filters=res_block.conv_kernel1,
-                                       strides=[1, 1, 1, 1],
-                                       padding="SAME") + \
-                          res_block.biases1
-            conv_output = tf.nn.leaky_relu(features=conv_output)
+            block_num = len(tower_hidden_outputs)
 
-            conv_output = tf.nn.conv2d(input=conv_output,
-                                       filters=res_block.conv_kernel2,
-                                       strides=[1, 1, 1, 1],
-                                       padding="SAME") + \
-                          res_block.biases2
-            conv_features = conv_features + tf.nn.leaky_relu(features=conv_output)
+            conv_output1_linear = tf.nn.conv2d(input=tower_hidden_outputs[-1],
+                                                filters=res_block.conv_kernel1,
+                                                strides=[1, 1, 1, 1],
+                                                padding="SAME",
+                                                name="res_block_{0}_conv1".format(block_num)) + \
+                                   res_block.biases1
+            conv_output1 = tf.nn.leaky_relu(features=conv_output1_linear,
+                                            name="res_block_{0}_1".format(block_num))
+
+            conv_output2_linear = tf.nn.conv2d(input=conv_output1,
+                                               filters=res_block.conv_kernel2,
+                                               strides=[1, 1, 1, 1],
+                                               padding="SAME",
+                                               name="res_block_{0}_conv2".format(block_num)) + \
+                                  res_block.biases2
+            conv_output2 = tower_hidden_outputs[-1] + \
+                tf.nn.leaky_relu(features=conv_output2_linear,
+                                 name="res_block_{0}_2".format(block_num))
+
+            tower_hidden_outputs.append(conv_output2)
         
         # Policy output
-        policy_hidden_layer = tf.nn.conv2d(input=conv_features,
-                                           filters=self.policy_layer_.conv_kernel,
-                                           strides=[1, 1, 1, 1],
-                                           padding="SAME") + \
-                              self.policy_layer_.conv_biases
-        policy_hidden_layer = tf.nn.relu(features=policy_hidden_layer)
+        policy_hidden_linear = tf.nn.conv2d(input=tower_hidden_outputs[-1],
+                                            filters=self.policy_layer_.conv_kernel,
+                                            strides=[1, 1, 1, 1],
+                                            padding="SAME",
+                                            name="policy_fconv") + \
+                               self.policy_layer_.conv_biases
+        policy_hidden = tf.nn.relu(features=policy_hidden_linear,
+                                   name="policy_plane")
 
-        policy_hidden_layer = tf.reshape(tensor=policy_hidden_layer,
-                                         shape=[-1, INPUT_SIZE*INPUT_SIZE*2])
-        policy_logits = tf.matmul(a=policy_hidden_layer,
-                                  b=self.policy_layer_.weights) + \
+        policy_hidden_flattened = tf.reshape(tensor=policy_hidden,
+                                             shape=[-1, INPUT_SIZE*INPUT_SIZE*2],
+                                             name="policy_plane_flattened")
+        policy_logits = tf.matmul(a=policy_hidden_flattened,
+                                  b=self.policy_layer_.weights,
+                                  name="policy_logits_matmul") + \
                      self.policy_layer_.biases
-        policy = tf.nn.softmax(logits=policy_logits)
+        policy = tf.nn.softmax(logits=policy_logits, name="policy_prob")
 
         # Value output
-        value_hidden_layer = tf.nn.conv2d(input=conv_features,
-                                           filters=self.value_layer_.conv_kernel,
-                                           strides=[1, 1, 1, 1],
-                                           padding="SAME") + \
-                              self.value_layer_.conv_biases
-        value_hidden_layer = tf.nn.relu(features=value_hidden_layer)
+        value_hidden1_linear = tf.nn.conv2d(input=tower_hidden_outputs[-1],
+                                            filters=self.value_layer_.conv_kernel,
+                                            strides=[1, 1, 1, 1],
+                                            padding="SAME",
+                                            name="value_fconv") + \
+                               self.value_layer_.conv_biases
+        value_hidden1 = tf.nn.relu(features=value_hidden1_linear,
+                                   name="value_plane")
 
-        value_hidden_layer = tf.reshape(tensor=value_hidden_layer,
-                                        shape=[-1, INPUT_SIZE*INPUT_SIZE])
-        value_hidden_layer2 = tf.matmul(a=value_hidden_layer,
-                                        b=self.value_layer_.weights1) + \
-                              self.value_layer_.biases1
-        value_hidden_layer2 = tf.nn.relu(features=value_hidden_layer2)
+        value_hidden_flattened = tf.reshape(tensor=value_hidden1,
+                                            shape=[-1, INPUT_SIZE*INPUT_SIZE],
+                                            name="value_plane_flattened")
+        value_hidden2_linear = tf.matmul(a=value_hidden_flattened,
+                                         b=self.value_layer_.weights1,
+                                         name="value_summary_matmul") + \
+                               self.value_layer_.biases1
+        value_hidden2 = tf.nn.relu(features=value_hidden2_linear,
+                                   name="value_summary")
 
-        value = tf.matmul(a=value_hidden_layer2,
-                          b=self.value_layer_.weights2) + \
-                self.value_layer_.biases2
-        value = tf.tanh(x=value)
-        value = tf.reshape(tensor=value, shape=[-1])
+        value_score = tf.matmul(a=value_hidden2,
+                                b=self.value_layer_.weights2,
+                                name="value_score_matmul") + \
+                      self.value_layer_.biases2
+        value = tf.tanh(x=value_score, name="value_map_to_range")
+        value = tf.reshape(tensor=value,
+                           shape=[-1],
+                           name="value_reshape")
 
         return policy, value, policy_logits
     
@@ -341,5 +388,5 @@ class GomokuCnnResNetSharedTower(tf.Module):
             tf.losses.mse(y_true=values, y_pred=pred_values)
 
         loss = policy_loss + value_loss
-        
+
         return loss, policy_loss, value_loss
