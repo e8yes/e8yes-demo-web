99054326d092fcc7a8c10dbc723318dc97462a84
Chifeng Wen <daviesx66@gmail.com>
2021-02-28 00:56:45 -0800

    Design the handcrafted SHL features which generally helps producing good heuristics.

---
+++ gomoku/agent/heuristics/shl_feature.cc
@@ -0,0 +1,277 @@
+/**
+ * e8yes demo web.
+ *
+ * <p>Copyright (C) 2020 Chifeng Wen {daviesx66@gmail.com}
+ *
+ * <p>This program is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * <p>This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * <p>You should have received a copy of the GNU General Public License along with this program. If
+ * not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <array>
+#include <cassert>
+#include <cmath>
+#include <cstdint>
+#include <optional>
+#include <tuple>
+#include <utility>
+#include <vector>
+
+#include "gomoku/agent/heuristics/contour.h"
+#include "gomoku/agent/heuristics/shl_feature.h"
+#include "gomoku/game/board_state.h"
+
+namespace e8 {
+namespace {
+
+enum LinkPatternDfaState { COUNT_PIECE, PUNCTUATION };
+
+std::tuple<int8_t, int8_t, bool> LinkageCountsAtWithDirection(int8_t x, int8_t y, int8_t dx,
+                                                              int8_t dy,
+                                                              StoneType compute_for_stone_type,
+                                                              GomokuBoardState const &board) {
+    int8_t raw_count = 0;
+    int8_t holes = 0;
+    bool blocked = false;
+    int8_t i = 1;
+
+    LinkPatternDfaState state = LinkPatternDfaState::COUNT_PIECE;
+
+    while (true) {
+        switch (state) {
+        case COUNT_PIECE: {
+            if (i > 6) {
+                return std::make_tuple(raw_count, holes, blocked);
+            }
+
+            int8_t shifted_x = x + i * dx;
+            int8_t shifted_y = y + i * dy;
+
+            if (shifted_x < 0 || shifted_x >= board.Width() || shifted_y < 0 ||
+                shifted_y >= board.Height() ||
+                (*board.ChessPieceStateAt(MovePosition(shifted_x, shifted_y)) !=
+                     StoneType::ST_NONE &&
+                 *board.ChessPieceStateAt(MovePosition(shifted_x, shifted_y)) !=
+                     compute_for_stone_type)) {
+                blocked = true;
+                return std::make_tuple(raw_count, holes, blocked);
+            } else if (*board.ChessPieceStateAt(MovePosition(shifted_x, shifted_y)) ==
+                       StoneType::ST_NONE) {
+                state = LinkPatternDfaState::PUNCTUATION;
+            } else {
+                ++raw_count;
+                ++i;
+            }
+            break;
+        }
+        case PUNCTUATION: {
+            ++i;
+
+            int8_t shifted_x = x + i * dx;
+            int8_t shifted_y = y + i * dy;
+
+            if (shifted_x < 0 || shifted_x >= board.Width() || shifted_y < 0 ||
+                shifted_y >= board.Height() ||
+                *board.ChessPieceStateAt(MovePosition(shifted_x, shifted_y)) !=
+                    compute_for_stone_type ||
+                *board.ChessPieceStateAt(MovePosition(shifted_x, shifted_y)) ==
+                    StoneType::ST_NONE) {
+                return std::make_tuple(raw_count, holes, blocked);
+            } else {
+                ++holes;
+                state = LinkPatternDfaState::COUNT_PIECE;
+            }
+            break;
+        }
+        }
+    }
+
+    assert(false);
+}
+
+LinkStats LinkageAtWithDirection(int8_t x, int8_t y, int8_t dx, int8_t dy,
+                                 StoneType compute_for_stone_type, GomokuBoardState const &board) {
+    auto [raw_count1, holes1, blocked1] =
+        LinkageCountsAtWithDirection(x, y, dx, dy, compute_for_stone_type, board);
+
+    auto [raw_count2, holes2, blocked2] =
+        LinkageCountsAtWithDirection(x, y, -dx, -dy, compute_for_stone_type, board);
+
+    return LinkStats(raw_count1 + raw_count2, holes1 + holes2, blocked1 + blocked2);
+}
+
+std::array<LinkStats, 4> LinkagesAt(int8_t x, int8_t y, StoneType compute_for_stone_type,
+                                    GomokuBoardState const &board) {
+    LinkStats horizontal =
+        LinkageAtWithDirection(x, y, /*dx=*/1, /*dy=*/0, compute_for_stone_type, board);
+
+    LinkStats vertical =
+        LinkageAtWithDirection(x, y, /*dx=*/0, /*dy =*/1, compute_for_stone_type, board);
+
+    LinkStats diagnal =
+        LinkageAtWithDirection(x, y, /*dx=*/1, /*dy=*/1, compute_for_stone_type, board);
+
+    LinkStats counter_diagnal =
+        LinkageAtWithDirection(x, y, /*dx=*/1, /*dy=*/-1, compute_for_stone_type, board);
+
+    return std::array<LinkStats, 4>({horizontal, vertical, diagnal, counter_diagnal});
+}
+
+float LinkageToAdjustedCount(LinkStats const &linkage, StoneType compute_for_stone_type,
+                             std::optional<StoneType> next_move_stone_type) {
+    float move_advantage;
+    if (next_move_stone_type.has_value()) {
+        if (compute_for_stone_type == *next_move_stone_type) {
+            move_advantage = 1;
+        } else {
+            move_advantage = 0.5f;
+        }
+    } else {
+        move_advantage = 1.0f;
+    }
+
+    float adjusted_count = std::max(0.0f, linkage.raw_count + move_advantage -
+                                              0.5f * linkage.holes - linkage.blockage);
+
+    return adjusted_count;
+}
+
+std::pair<float, float> ShlCount(int8_t x, int8_t y, StoneType compute_for_stone_type,
+                                 std::optional<StoneType> next_move_stone_type,
+                                 GomokuBoardState const &board) {
+    std::array<LinkStats, 4> linkages = LinkagesAt(x, y, compute_for_stone_type, board);
+
+    std::array<float, 4> adjusted_counts;
+    for (unsigned i = 0; i < 4; ++i) {
+        adjusted_counts[i] =
+            LinkageToAdjustedCount(linkages[i], compute_for_stone_type, next_move_stone_type);
+    }
+
+    std::sort(adjusted_counts.begin(), adjusted_counts.end(), std::greater<float>());
+
+    float primary_shl_count = std::pow(adjusted_counts[0], 2.5f);
+    float secondary_shl_count = std::pow(adjusted_counts[1], 2.5f);
+
+    return std::make_pair(primary_shl_count, secondary_shl_count);
+}
+
+struct PositionAndShlScore {
+    PositionAndShlScore(MovePosition const &position, ShlComponents const &shl_components);
+
+    bool operator>(PositionAndShlScore const &rhs) const;
+
+    MovePosition position;
+    ShlComponents shl_components;
+    float shl_score;
+};
+
+PositionAndShlScore::PositionAndShlScore(MovePosition const &position,
+                                         ShlComponents const &shl_components)
+    : position((position)), shl_components(shl_components),
+      shl_score(shl_components.primary_shl_count_black + shl_components.secondary_shl_count_black +
+                shl_components.primary_shl_count_white + shl_components.secondary_shl_count_white) {
+}
+
+bool PositionAndShlScore::operator>(PositionAndShlScore const &rhs) const {
+    return shl_score > rhs.shl_score;
+}
+
+void NonTopKSuppression(unsigned k, ShlFeatures *shl_map) {
+    std::vector<PositionAndShlScore> shl_scores;
+    for (auto &[position, raw_score] : shl_map->raw_map) {
+        shl_scores.push_back(PositionAndShlScore(position, raw_score));
+    }
+
+    std::sort(shl_scores.begin(), shl_scores.end(), std::greater<PositionAndShlScore>());
+
+    shl_map->shl_score_total = 0.0f;
+    for (unsigned i = 0; i < k && i < shl_scores.size(); ++i) {
+        shl_map->shl_score_total += shl_scores[i].shl_score;
+        shl_map->normalized_top_k_map.push_back(
+            std::make_pair(shl_scores[i].position, shl_scores[i].shl_components));
+        shl_map->top_k_positions.push_back(shl_scores[i].position);
+    }
+}
+
+void Normalize(ShlFeatures *shl_map) {
+    if (shl_map->raw_map.empty()) {
+        return;
+    }
+
+    assert(shl_map->shl_score_total > 0.0f);
+
+    float norm_factor = 1.0f / shl_map->shl_score_total;
+    for (auto &[_, shl_components] : shl_map->normalized_top_k_map) {
+        shl_components.primary_shl_count_black *= norm_factor;
+        shl_components.secondary_shl_count_black *= norm_factor;
+
+        shl_components.primary_shl_count_white *= norm_factor;
+        shl_components.secondary_shl_count_white *= norm_factor;
+    }
+}
+
+} // namespace
+
+LinkStats::LinkStats(int8_t raw_count, int8_t holes, bool blockage)
+    : raw_count(raw_count), holes(holes), blockage(blockage) {}
+
+ShlComponents::ShlComponents()
+    : primary_shl_count_black(0.0f), secondary_shl_count_black(0.0f), primary_shl_count_white(0.0f),
+      secondary_shl_count_white(0.0f) {}
+
+ShlComponents::ShlComponents(float primary_shl_count_black, float secondary_shl_count_black,
+                             float primary_shl_count_white, float secondary_shl_count_white)
+    : primary_shl_count_black(primary_shl_count_black),
+      secondary_shl_count_black(secondary_shl_count_black),
+      primary_shl_count_white(primary_shl_count_white),
+      secondary_shl_count_white(secondary_shl_count_white) {}
+
+ShlFeatures::ShlFeatures(unsigned width, unsigned height) : width(width), height(height) {}
+
+ShlFeatures ComputeShlFeatures(GomokuBoardState const &board,
+                               std::optional<StoneType> next_move_stone_type, unsigned top_k) {
+    assert(top_k >= 1);
+
+    ShlFeatures shl_map(board.Width(), board.Height());
+
+    for (int8_t y = 0; y < board.Height(); ++y) {
+        for (int8_t x = 0; x < board.Width(); ++x) {
+            if (!IsDoubleContour(x, y, board)) {
+                continue;
+            }
+
+            auto [primary_black, secondary_black] = ShlCount(
+                x, y, /*compute_for_stone_type=*/StoneType::ST_BLACK, next_move_stone_type, board);
+            auto [primary_white, secondary_white] = ShlCount(
+                x, y, /*compute_for_stone_type=*/StoneType::ST_WHITE, next_move_stone_type, board);
+
+            shl_map.raw_map.push_back(
+                std::make_pair(MovePosition(x, y), ShlComponents(primary_black, secondary_black,
+                                                                 primary_white, secondary_white)));
+        }
+    }
+
+    NonTopKSuppression(top_k, &shl_map);
+    Normalize(&shl_map);
+
+    return shl_map;
+}
+
+std::vector<ShlComponents> ToDenseShlMap(ShlFeatures const &feature_map) {
+    std::vector<ShlComponents> dense_map(feature_map.width * feature_map.height);
+
+    for (auto const &[pos, shl_components] : feature_map.normalized_top_k_map) {
+        dense_map[pos.x + pos.y * feature_map.width] = shl_components;
+    }
+
+    return dense_map;
+}
+
+} // namespace e8
---
+++ gomoku/agent/heuristics/shl_feature.h
@@ -0,0 +1,125 @@
+/**
+ * e8yes demo web.
+ *
+ * <p>Copyright (C) 2020 Chifeng Wen {daviesx66@gmail.com}
+ *
+ * <p>This program is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * <p>This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * <p>You should have received a copy of the GNU General Public License along with this program. If
+ * not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef SHL_FEATURE_H
+#define SHL_FEATURE_H
+
+#include <cstdint>
+#include <optional>
+#include <utility>
+#include <vector>
+
+#include "gomoku/game/board_state.h"
+
+namespace e8 {
+
+/**
+ * @brief The LinkStats struct Statistics for a link viewed at certain position. Each position
+ * involves exactly four potential links laid in horizontal, vertical, diagnal and counter diagnal
+ * direction centered at the viewer position. A link: cannot have any consecutive holes; within a
+ * 5-cell radius from the viewer position; consists of only one type of stones.
+ */
+struct LinkStats {
+    LinkStats(int8_t raw_count, int8_t holes, bool blockage);
+
+    // The number of stones in a link.
+    int8_t raw_count;
+
+    // The number of holes in a link.
+    int8_t holes;
+
+    // How many blockage is there in a link. Each link can have 0 to 2 blockages (open, half-closed
+    // and fully closed).
+    bool blockage;
+};
+
+/**
+ * @brief The ShlComponents struct Score features for the primary and secondary links for each stone
+ * type.
+ */
+struct ShlComponents {
+    ShlComponents();
+    ShlComponents(float primary_shl_count_black, float secondary_shl_count_black,
+                  float primary_shl_count_white, float secondary_shl_count_white);
+
+    float primary_shl_count_black;
+    float secondary_shl_count_black;
+    float primary_shl_count_white;
+    float secondary_shl_count_white;
+};
+
+/**
+ * @brief The ShlMap struct A sparse representation of the SHL features.
+ */
+struct ShlFeatures {
+    ShlFeatures(unsigned width, unsigned height);
+
+    // Width and height of the feature map. Should be in the same size of the board.
+    unsigned width;
+    unsigned height;
+
+    // Sparse 2D map storing the raw SHL components.
+    std::vector<std::pair<MovePosition, ShlComponents>> raw_map;
+
+    // Sparse 2D map storing the suppressed and normalized SHL components.
+    std::vector<std::pair<MovePosition, ShlComponents>> normalized_top_k_map;
+
+    // The ordering of the following lists indicates the rank of those top K ShlComponents pointed
+    // to by their positions. This list may not get filled up in the case when there isn't K SHL
+    // candidate scores.
+    std::vector<MovePosition> top_k_positions;
+
+    // Sum of raw component scores at those unsupressed SHL locations. The total will be
+    // zero if the map is empty.
+    float shl_score_total;
+};
+
+/**
+ * @brief ComputeShlFeatures Extract the Suggestive Hotspot Links (SHL) features from the board. The
+ * SHL features are extracted by looking at all possible links from all the double contour
+ * positions for both stone types (See LinkStats for how a link is defined). An adjusted count is
+ * then assigned for each link and stone type and is caluated as follow:
+ *
+ * adjusted_count = |raw_count + move_advantage - 0.5*holes - blockage|^2.5. Move advantage is set
+ * to 1 if the next_move_stone_type is the same as the adjusted count's stone type. Otherwise, it is
+ * set to 0.5. If the next_move_stone_type is not supplied, move_advantage is default to 1.
+ *
+ * For each stone typep, the top 2 links are selected at each position based on this adjusted count
+ * and are called the primary and secondary counts, respectively. These counts constitutes the
+ * raw_map.
+ *
+ * Then it computes the SHL score using those adjusted counts for each position:
+ *
+ * shl_score = primary_count_black + secondary_count_black + primary_count_white +
+ * secondary_count_white
+ *
+ * Eventually, it ranks each position's adjusted count by the shl_score and selects the top K
+ * positions. Other positions are erased. With the unsuppressed positions, the adjusted counts are
+ * normalized by the sum of the remaining SHL scores. This gives the final SHL map.
+ */
+ShlFeatures ComputeShlFeatures(GomokuBoardState const &board,
+                               std::optional<StoneType> next_move_stone_type, unsigned top_k);
+
+/**
+ * @brief ToDenseShlMap The original SHL map is stored in sparse format. This function converts the
+ * map to a dense representation.
+ */
+std::vector<ShlComponents> ToDenseShlMap(ShlFeatures const &feature_map);
+
+} // namespace e8
+
+#endif // SHL_FEATURE_H
