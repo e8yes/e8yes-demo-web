c90c97fb0b89c3f2d5899be3e4473e729077fa67
Chifeng Wen <daviesx66@gmail.com>
2021-03-01 04:06:43 -0800

    Add the mlpack library as part of the development dependency.

--- gomoku/install_dev_pkgs.sh
+++ gomoku/install_dev_pkgs.sh
@@ -3,6 +3,9 @@
 echo "Installing C++ protobuf development packages..."
 sudo apt install -y libprotobuf-dev
 
+echo "Installing mlpack development packages..."
+sudo apt install -y libmlpack-dev
+
 echo "Installing python numpy packages..."
 sudo apt install -y python3-numpy
 
