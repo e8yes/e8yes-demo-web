90c57221f4118712f21deadc254c3d117ff3fb2e
Chifeng Wen <daviesx66@gmail.com>
2021-03-03 05:27:18 -0800

    Add a script to evaluate the policy model.

---
+++ gomoku/agent_classroom/tfmodel/argmax_policy_accuracy.py
@@ -0,0 +1,145 @@
+#!/bin/python3
+
+import argparse
+import os
+import logging
+import tensorflow as tf
+import numpy as np
+
+from batch_generator import BatchGenerator
+from poly_functions import RequireShlFeatures
+from poly_functions import LoadModel
+from poly_functions import ReadModelName
+from poly_functions import ReadBoardSize
+
+def ArgmaxPolicyAccuracy(
+    model: any,
+    batch_gen: BatchGenerator,
+    last_k_steps: int,
+    require_shl: bool) -> float:
+    boards, \
+    game_phases, \
+    next_move_stone_types, \
+    shl_maps, \
+    top_shl_features, \
+    policies, \
+    _ = \
+        batch_gen.NextBatch(
+            batch_size=100000,
+            sample_from=0,
+            training_data=False,
+            last_k_steps=last_k_steps)
+
+    policy_preds = None
+    if require_shl:
+        policy_preds, _ = model(
+            boards,
+            game_phases,
+            next_move_stone_types,
+            shl_maps,
+            top_shl_features)
+    else:
+        policy_preds, _ = model(
+            boards,
+            game_phases,
+            next_move_stone_types)
+
+    action_ground_truth = np.argmax(a=policies, axis=1)
+
+    action_preds = np.argmax(a=policy_preds, axis=1)
+
+    shl_scores = np.sum(a=shl_maps, axis=3)
+    flat_shl_scores = np.reshape(
+        a=shl_scores, newshape=(-1, 11*11), order="F")
+    shl_action_preds = np.argmax(a=flat_shl_scores, axis=1)
+
+    num_correct_heuristics = np.sum(
+        a=action_preds == action_ground_truth, axis=0)
+    num_correct_shl = np.sum(
+        a=shl_action_preds == action_ground_truth, axis=0)
+
+    return num_correct_heuristics / policies.shape[0], \
+           num_correct_shl / policies.shape[0], \
+           policies.shape[0]
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Evaluates the model by checking if the argmax action "
+                    "aligns with the data set's.")
+    parser.add_argument("--model_input_path",
+        type=str,
+        help="A path to the model to be evaluated. Do not specify a file name.")
+    parser.add_argument("--k",
+        type=int,
+        help="Parameter of how many states to look back from the game's end state.")
+    parser.add_argument("--db_host",
+        type=str,
+        help="Host name pointing to the database storing the latest game data.")
+    parser.add_argument("--db_name",
+        type=str,
+        help="Name of the database storing the latest game data.")
+    parser.add_argument("--db_user",
+        type=str,
+        help="User of the database that has access to the latest game data.")
+    parser.add_argument("--db_pass",
+        type=str,
+        help="Password of the user that has access to the latest game data.")
+
+    logging.basicConfig(level=logging.INFO, 
+                        format="%(asctime)s %(levelname)s %(message)s")
+
+    args = parser.parse_args()
+    model_input_path = args.model_input_path
+    k = args.k
+    db_host = args.db_host
+    db_name = args.db_name
+    db_user = args.db_user
+    db_pass = args.db_pass
+
+    if model_input_path is None:
+        logging.error("Argument model_input_path is required.")
+        parser.print_help()
+        exit(-1)
+    if k is None:
+        logging.error("Argument k is required.")
+        parser.print_help()
+        exit(-1)
+    if db_host is None:
+        logging.error("Argument db_host is required.")
+        parser.print_help()
+        exit(-1)
+    if db_name is None:
+        logging.error("Argument db_name is required.")
+        parser.print_help()
+        exit(-1)
+    if db_user is None:
+        logging.error("Argument db_user is required.")
+        parser.print_help()
+        exit(-1)
+    if db_pass is None:
+        logging.error("Argument db_pass is required.")
+        parser.print_help()
+        exit(-1)
+
+    model = LoadModel(model_import_path=model_input_path)
+    model_name = ReadModelName(
+        model_import_path=model_input_path)
+    board_size = ReadBoardSize(model_name=model_name)
+
+    gen = BatchGenerator(
+        board_size=board_size,
+        num_data_entries=None,
+        db_name=db_name,
+        db_host=db_host,
+        db_port=5432,
+        db_user=db_user,
+        db_pass=db_pass)
+
+    heuristics_acc, shl_acc, n = ArgmaxPolicyAccuracy(
+        model=model,
+        batch_gen=gen,
+        last_k_steps=k,
+        require_shl=RequireShlFeatures(model_name))
+    logging.info("ArgmaxPolicyAccuracy={0}, {1}; n={2}"\
+        .format(heuristics_acc, shl_acc, n))
