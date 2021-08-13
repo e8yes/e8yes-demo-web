9c64653dd17d594c80c379a048cbf94962c6aef8
Chifeng Wen <daviesx66@gmail.com>
2021-08-12 20:34:05 -0700

    Adds an object called ElectionCommittee to help shape election rules and facilitate elections.

--- replication/raft/election.cc
+++ replication/raft/election.cc
@@ -15,13 +15,93 @@
  * not, see <http://www.gnu.org/licenses/>.
  */
 
+#include <chrono>
+#include <grpcpp/grpcpp.h>
+#include <memory>
 #include <mutex>
 #include <optional>
 
+#include "common/thread/thread_pool.h"
+#include "common/time_util/time_util.h"
+#include "proto_cc/raft.pb.h"
+#include "proto_cc/service_raft.grpc.pb.h"
+#include "proto_cc/service_raft.pb.h"
 #include "replication/raft/common_types.h"
 #include "replication/raft/election.h"
+#include "replication/raft/peer_set.h"
 
 namespace e8 {
+namespace {
+
+class CollectVoteArgs : public TaskStorageInterface {
+  public:
+    CollectVoteArgs(RaftTerm for_term, RaftMachineAddress const &candidate_machine_address,
+                    LogSourceLiveness const &candidate_log_liveness, RaftService::Stub *target_stub,
+                    TimeIntervalMillis election_timeout_millis,
+                    std::shared_ptr<RaftElectionCommittee::Result> const &election_result);
+    virtual ~CollectVoteArgs() override;
+
+    RaftTerm for_term;
+    RaftMachineAddress candidate_machine_address;
+    LogSourceLiveness candidate_log_liveness;
+    RaftService::Stub *target_stub;
+    TimeIntervalMillis election_timeout_millis;
+    std::shared_ptr<RaftElectionCommittee::Result> election_result;
+};
+
+CollectVoteArgs::CollectVoteArgs(
+    RaftTerm for_term, RaftMachineAddress const &candidate_machine_address,
+    LogSourceLiveness const &candidate_log_liveness, RaftService::Stub *target_stub,
+    TimeIntervalMillis election_timeout_millis,
+    std::shared_ptr<RaftElectionCommittee::Result> const &election_result)
+    : for_term(for_term), candidate_machine_address(candidate_machine_address),
+      candidate_log_liveness(candidate_log_liveness), target_stub(target_stub),
+      election_timeout_millis(election_timeout_millis), election_result(election_result) {}
+
+CollectVoteArgs::~CollectVoteArgs() {}
+
+class CollectVoteTask : public TaskInterface {
+  public:
+    CollectVoteTask();
+    ~CollectVoteTask();
+
+    void Run(TaskStorageInterface *args) const override;
+
+    bool DropResourceOnCompletion() const override;
+
+  private:
+    std::shared_ptr<RaftElectionCommittee::Result> election_result_;
+};
+
+CollectVoteTask::CollectVoteTask() {}
+
+CollectVoteTask::~CollectVoteTask() {}
+
+void CollectVoteTask::Run(TaskStorageInterface *data) const {
+    auto args = static_cast<CollectVoteArgs const *>(data);
+
+    GrantVoteRequest request;
+    request.set_candidate_machine_address(args->candidate_machine_address);
+    request.set_candidate_term(args->for_term);
+    *request.mutable_candidate_liveness() = args->candidate_log_liveness;
+
+    GrantVoteResponse response;
+
+    grpc::ClientContext context;
+    context.set_deadline(std::chrono::system_clock::now() +
+                         std::chrono::milliseconds(args->election_timeout_millis));
+
+    grpc::Status status = args->target_stub->GrantVote(&context, request, &response);
+    if (status.ok() || !response.vote_granted()) {
+        return;
+    }
+
+    args->election_result->AddVote();
+}
+
+bool CollectVoteTask::DropResourceOnCompletion() const { return false; }
+
+} // namespace
 
 RaftVotingRecord::RaftVotingRecord() {}
 
@@ -36,5 +116,45 @@ void RaftVotingRecord::VoteFor(RaftMachineAddress candidate_address) {
 }
 
 std::optional<RaftMachineAddress> RaftVotingRecord::LastVotedFor() const { return last_voted_for_; }
+
+RaftElectionCommittee::RaftElectionCommittee(RaftPeerSet *peers, unsigned quorum_size,
+                                             TimeIntervalMillis election_timeout_millis)
+    : peers_(peers), quorum_size_(quorum_size), election_timeout_millis_(election_timeout_millis),
+      thread_pool_(peers->PeerCount()) {}
+
+std::shared_ptr<RaftElectionCommittee::Result>
+RaftElectionCommittee::StartElection(RaftTerm for_term,
+                                     RaftMachineAddress candidate_machine_address,
+                                     LogSourceLiveness const &candidate_log_liveness) {
+    auto result = std::make_shared<Result>(quorum_size_);
+
+    auto collect_vote_task = std::make_shared<CollectVoteTask>();
+
+    for (auto const &[peer_address, peer_stub] : *peers_) {
+        if (peer_address == candidate_machine_address) {
+            // Votes for ourselves.
+            result->AddVote();
+            continue;
+        }
+
+        auto args = std::make_unique<CollectVoteArgs>(for_term, candidate_machine_address,
+                                                      candidate_log_liveness, peer_stub.get(),
+                                                      election_timeout_millis_, result);
+        thread_pool_.Schedule(collect_vote_task, std::move(args));
+    }
+
+    return result;
+}
+
+RaftElectionCommittee::Result::Result(unsigned quorum_size) : quorum_size_(quorum_size) {}
+
+RaftElectionCommittee::Result::~Result() {}
+
+bool RaftElectionCommittee::Result::Won() const { return num_votes_ >= quorum_size_; }
+
+void RaftElectionCommittee::Result::AddVote() {
+    std::lock_guard<std::mutex> guard(mu_);
+    ++num_votes_;
+}
 
 } // namespace e8
--- replication/raft/election.h
+++ replication/raft/election.h
@@ -18,10 +18,15 @@
 #ifndef RAFT_ELECTION_H
 #define RAFT_ELECTION_H
 
+#include <memory>
 #include <mutex>
 #include <optional>
 
+#include "common/thread/thread_pool.h"
+#include "common/time_util/time_util.h"
+#include "proto_cc/raft.pb.h"
 #include "replication/raft/common_types.h"
+#include "replication/raft/peer_set.h"
 
 namespace e8 {
 
@@ -50,6 +55,72 @@ class RaftVotingRecord {
   private:
     std::mutex lock_;
     std::optional<RaftMachineAddress> last_voted_for_;
+};
+
+/**
+ * @brief The RaftElectionCommittee class Sets the election rules and provides means to conduct
+ * elections.
+ */
+class RaftElectionCommittee {
+  public:
+    /**
+     * @brief The Result class Tracks and tallies the election result.
+     */
+    class Result {
+      public:
+        explicit Result(unsigned quorum_size);
+        ~Result();
+
+        /**
+         * @brief Won Checks if the candidate has won the election by now.
+         */
+        bool Won() const;
+
+        /**
+         * @brief AddVote The candidates gets one more vote.
+         */
+        void AddVote();
+
+      private:
+        std::mutex mu_;
+        unsigned num_votes_;
+        unsigned const quorum_size_;
+    };
+
+    /**
+     * @brief RaftElectionCommittee Sets up election rules.
+     *
+     * @param peers Connection to each peer's Raft server.
+     * @param quorum_size The number of votes to make up a quorum, should be at least
+     * floor(peers->PeerCount()/2) + 1
+     * @param election_timeout_millis The *recommended* number of milliseconds to time out a
+     * GrantVote() RPC.
+     */
+    RaftElectionCommittee(RaftPeerSet *peers, unsigned quorum_size,
+                          TimeIntervalMillis election_timeout_millis);
+    ~RaftElectionCommittee();
+
+    /**
+     * @brief StartElection Starts a new election for the specified term. It's OK to run multiple
+     * elections at the same term. However, each node other than itself will only be able to cast
+     * ballot at most once, and this function doesn't cache past election result. This call does not
+     * block. It's up to the caller to determine when it is appropiate to check the election result.
+     *
+     * @param for_term The term to start election for.
+     * @param candidate_machine_address The machine address of the candidate.
+     * @param candidate_log_liveness The log's freshness of the candidate node.
+     *
+     * @return A result object allowing the caller to check the election result whenever it wants.
+     */
+    std::shared_ptr<Result> StartElection(RaftTerm for_term,
+                                          RaftMachineAddress candidate_machine_address,
+                                          LogSourceLiveness const &candidate_log_liveness);
+
+  private:
+    RaftPeerSet *peers_;
+    unsigned const quorum_size_;
+    TimeIntervalMillis const election_timeout_millis_;
+    ThreadPool thread_pool_;
 };
 
 } // namespace e8
