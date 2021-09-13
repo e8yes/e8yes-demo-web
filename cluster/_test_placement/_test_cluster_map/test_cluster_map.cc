7694022d1be38993ebddf912302f1a1531e1d430
Chifeng Wen <daviesx66@gmail.com>
2021-09-13 07:03:17 -0700

    Tests the resource placement feature.

--- cluster/_test_placement/_test_cluster_map/test_cluster_map.cc
+++ cluster/_test_placement/_test_cluster_map/test_cluster_map.cc
@@ -15,23 +15,32 @@
  * not, see <http://www.gnu.org/licenses/>.
  */
 
-#include <iostream>
+#include <string>
+#include <unordered_set>
+#include <vector>
 
 #include "cluster/placement/cluster_map.h"
+#include "cluster/placement/common_types.h"
 #include "common/unit_test_util/unit_test_util.h"
 #include "proto_cc/cluster.pb.h"
+#include "proto_cc/machine.pb.h"
+
+namespace {
 
 e8::ClusterMapData Proto() {
     e8::ClusterMapData proto;
 
     e8::ClusterTreeNode root;
+    root.set_hierarchy(e8::ClusterTreeNode::ROOT);
     *root.mutable_bucket()->mutable_list_bucket()->add_child_labels() = "row1";
     *root.mutable_bucket()->mutable_list_bucket()->add_child_labels() = "row2";
 
     e8::ClusterTreeNode row1;
     e8::ClusterTreeNode row2;
+    row1.set_hierarchy(e8::ClusterTreeNode::ROW);
     *row1.mutable_bucket()->mutable_list_bucket()->add_child_labels() = "machine1";
     *row1.mutable_bucket()->mutable_list_bucket()->add_child_labels() = "machine2";
+    row2.set_hierarchy(e8::ClusterTreeNode::ROW);
     *row2.mutable_bucket()->mutable_list_bucket()->add_child_labels() = "machine3";
     *row2.mutable_bucket()->mutable_list_bucket()->add_child_labels() = "machine4";
 
@@ -39,18 +48,22 @@ e8::ClusterMapData Proto() {
     e8::ClusterTreeNode machine2;
     e8::ClusterTreeNode machine3;
     e8::ClusterTreeNode machine4;
+    machine1.set_hierarchy(e8::ClusterTreeNode::MACHINE);
     machine1.mutable_machine()->set_address("192.168.1.201");
     machine1.mutable_machine()->set_reachable(true);
     machine1.mutable_machine()->mutable_capabilities()->set_cpu(1);
 
+    machine2.set_hierarchy(e8::ClusterTreeNode::MACHINE);
     machine2.mutable_machine()->set_address("192.168.1.202");
     machine2.mutable_machine()->set_reachable(true);
     machine2.mutable_machine()->mutable_capabilities()->set_ram(1);
 
+    machine3.set_hierarchy(e8::ClusterTreeNode::MACHINE);
     machine3.mutable_machine()->set_address("192.168.1.203");
     machine3.mutable_machine()->set_reachable(true);
     machine3.mutable_machine()->mutable_capabilities()->set_storage(1);
 
+    machine4.set_hierarchy(e8::ClusterTreeNode::MACHINE);
     machine4.mutable_machine()->set_address("192.168.1.204");
     machine4.mutable_machine()->set_reachable(true);
     machine4.mutable_machine()->mutable_capabilities()->set_coral(1);
@@ -66,6 +79,14 @@ e8::ClusterMapData Proto() {
     return proto;
 }
 
+bool OneOfMachineSet(e8::Machine const &machine,
+                     std::unordered_set<std::string> const &machine_set) {
+    auto it = machine_set.find(machine.address());
+    return it != machine_set.end();
+}
+
+} // namespace
+
 bool ConstructAndExportTest() {
     e8::ClusterMapData proto = Proto();
 
@@ -75,9 +96,62 @@ bool ConstructAndExportTest() {
     return true;
 }
 
+bool ValidPlacementTest() {
+    e8::ClusterMapData proto = Proto();
+
+    e8::ResourceDescriptor resource(/*key=*/"a");
+
+    e8::ClusterMap cluster_map(proto);
+    std::vector<e8::Machine> machines = cluster_map.TakeRootFor(resource)
+                                            .Select(e8::ClusterTreeNode::ROW, /*num_items=*/2)
+                                            .Select(e8::ClusterTreeNode::MACHINE, /*num_items=*/1)
+                                            .Emit();
+
+    TEST_CONDITION(machines.size() == 2);
+    TEST_CONDITION(machines[0].address() != machines[1].address());
+
+    std::unordered_set<std::string> row1_machines{"192.168.1.201", "192.168.1.202"};
+    std::unordered_set<std::string> row2_machines{"192.168.1.203", "192.168.1.204"};
+    TEST_CONDITION((OneOfMachineSet(machines[0], row1_machines) &&
+                    OneOfMachineSet(machines[1], row2_machines)) ||
+                   (OneOfMachineSet(machines[0], row2_machines) &&
+                    OneOfMachineSet(machines[1], row1_machines)));
+
+    return true;
+}
+
+bool RepeatablePlacementTest() {
+    e8::ClusterMapData proto = Proto();
+
+    e8::ResourceDescriptor resource(/*key=*/"b");
+
+    e8::ClusterMap cluster_map(proto);
+    std::vector<e8::Machine> machines1 =
+        cluster_map.TakeRootFor(resource)
+            .Select(e8::ClusterTreeNode::ROW, /*num_replicas=*/2)
+            .Select(e8::ClusterTreeNode::MACHINE, /*num_replicas=*/1)
+            .Emit();
+
+    std::vector<e8::Machine> machines2 =
+        cluster_map.TakeRootFor(resource)
+            .Select(e8::ClusterTreeNode::ROW, /*num_replicas=*/2)
+            .Select(e8::ClusterTreeNode::MACHINE, /*num_replicas=*/1)
+            .Emit();
+
+    TEST_CONDITION(machines1.size() == 2);
+    TEST_CONDITION(machines2.size() == 2);
+
+    TEST_CONDITION(machines1[0].DebugString() == machines2[0].DebugString());
+    TEST_CONDITION(machines1[1].DebugString() == machines2[1].DebugString());
+
+    return true;
+}
+
 int main() {
     e8::BeginTestSuite("placement_cluster_map");
     e8::RunTest("ConstructAndExportTest", ConstructAndExportTest);
+    e8::RunTest("ValidPlacementTest", ValidPlacementTest);
+    e8::RunTest("RepeatablePlacementTest", RepeatablePlacementTest);
     e8::EndTestSuite();
     return 0;
 }
