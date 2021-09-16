0098a5ad4648c2a8fcbcf9289d6210494990801e
Chifeng Wen <daviesx66@gmail.com>
2021-09-16 08:07:01 -0700

    Adds the tree bucket implementation. Tree bucket has better selection performance compared to list bucket. It also reducing resource movement in both addition and removal.

--- cluster/placement/bucket.cc
+++ cluster/placement/bucket.cc
@@ -34,6 +34,17 @@ namespace {
 
 bool EnterWithProbability(size_t hash, float p) { return hash % 16384 < p * 16384; }
 
+WeightedCapabilities NegateCapabilities(WeightedCapabilities const &capabilities) {
+    WeightedCapabilities negated;
+
+    for (auto type : WeightedCapabilityTypes()) {
+        float value = GetCapabilityByType(type, capabilities);
+        SetCapabilityByType(type, -value, &negated);
+    }
+
+    return negated;
+}
+
 void AddCapabilities(WeightedCapabilities const &delta, WeightedCapabilities *target) {
     for (auto type : WeightedCapabilityTypes()) {
         float value = GetCapabilityByType(type, *target);
@@ -143,8 +154,6 @@ std::optional<ClusterTreeNodeLabel> ListBucket::Select(ResourceDescriptor const 
         return std::nullopt;
     }
 
-    unsigned jumps = rank + num_failures;
-
     std::vector<float> scores = scorer_->Score(children_capabilities_, resource);
     if (scores.empty()) {
         // None of the children is able to satisfy the resource.
@@ -168,8 +177,11 @@ std::optional<ClusterTreeNodeLabel> ListBucket::Select(ResourceDescriptor const 
     // At this point, we have shown the ith selection probability inductively.
     float sum_p = 0.0f;
 
+    std::string const jumps = std::to_string(rank + num_failures);
+    std::string const key_and_jumps = resource.key + jumps;
+
     for (unsigned i = 0; i < child_labels_.size(); ++i) {
-        std::string hash_key = resource.key + std::to_string(jumps) + child_labels_[i];
+        std::string hash_key = key_and_jumps + child_labels_[i];
         size_t random_variable = std::hash<std::string>()(hash_key);
 
         float p;
@@ -243,6 +255,325 @@ Bucket ListBucket::ToProto() const {
     return bucket;
 }
 
+TreeBucket::Node::Node() : id("1"), num_external_nodes(0), parent(nullptr) {}
+
+TreeBucket::Node::Node(Node *old_root)
+    : id(old_root->id + "0"), capabilities(old_root->capabilities),
+      num_external_nodes(old_root->num_external_nodes), parent(nullptr) {
+    assert(old_root != nullptr);
+
+    old_root->parent = this;
+}
+
+TreeBucket::Node::Node(Node *parent, unsigned depth, bool left_child)
+    : num_external_nodes(0), parent(parent) {
+    assert(parent != nullptr);
+
+    id = parent->id;
+    assert(id.length() > depth);
+
+    if (left_child) {
+        id[depth] = '0';
+    } else {
+        id[depth] = '1';
+    }
+}
+
+std::string TreeBucket::Node::Id() const { return id; }
+
+bool TreeBucket::Node::Internal() const { return left != nullptr && right != nullptr; }
+
+bool TreeBucket::Node::External() const { return left == nullptr && right == nullptr; }
+
+void TreeBucket::Node::SetChildren(std::unique_ptr<Node> &&left, std::unique_ptr<Node> &&right) {
+    assert(this->External());
+
+    assert(left != nullptr);
+    assert(right != nullptr);
+
+    this->left = std::move(left);
+    this->right = std::move(right);
+}
+
+bool TreeBucket::Node::Empty() const {
+    assert(this->External());
+    return !label.has_value();
+}
+
+void TreeBucket::Node::Clear() {
+    assert(this->External());
+
+    label.reset();
+
+    WeightedCapabilities deduction = NegateCapabilities(capabilities);
+
+    Node *current_ancestor = this;
+    do {
+        --current_ancestor->num_external_nodes;
+        e8::AddCapabilities(deduction, &current_ancestor->capabilities);
+
+        current_ancestor = current_ancestor->parent;
+    } while (current_ancestor != nullptr);
+}
+
+void TreeBucket::Node::Use(ClusterTreeNodeLabel const &label) {
+    assert(this->External());
+    this->label = label;
+
+    Node *current_ancestor = this;
+    do {
+        ++current_ancestor->num_external_nodes;
+        current_ancestor = current_ancestor->parent;
+    } while (current_ancestor != nullptr);
+}
+
+void TreeBucket::Node::AddCapabilities(WeightedCapabilities const &delta) {
+    assert(this->External());
+
+    Node *current_ancestor = this;
+    do {
+        e8::AddCapabilities(delta, &current_ancestor->capabilities);
+        current_ancestor = current_ancestor->parent;
+    } while (current_ancestor != nullptr);
+}
+
+unsigned TreeBucket::Node::ExternalNodeCount() const { return num_external_nodes; }
+
+ClusterTreeNodeLabel TreeBucket::Node::Label() const {
+    assert(!this->Empty());
+    return *label;
+}
+
+WeightedCapabilities TreeBucket::Node::Capabilities() const { return capabilities; }
+
+TreeBucket::Node *TreeBucket::Node::LeftChild() { return left.get(); }
+
+TreeBucket::Node *TreeBucket::Node::RightChild() { return right.get(); }
+
+TreeBucket::Node const *TreeBucket::Node::LeftChild() const { return left.get(); }
+
+TreeBucket::Node const *TreeBucket::Node::RightChild() const { return right.get(); }
+
+TreeBucket::TreeBucket(TreeBucketData const &data,
+                       std::unique_ptr<CapabilityScoreInterface> &&scorer)
+    : scorer_(std::move(scorer)), root_(std::make_unique<Node>()), depth_(0) {
+
+    for (auto const &child_label : data.child_labels()) {
+        bool added = this->AddChild(child_label);
+        assert(added);
+    }
+}
+
+TreeBucket::~TreeBucket() {}
+
+void TreeBucket::CollectBucketChildren(Node const *node, std::vector<Child> *children) const {
+    if (node->External() && !node->Empty()) {
+        children->push_back(Child(node->Label(), node->Capabilities()));
+        return;
+    }
+
+    assert(node->Internal());
+
+    // Internal nodes must have two children.
+    this->CollectBucketChildren(node->LeftChild(), children);
+    this->CollectBucketChildren(node->RightChild(), children);
+}
+
+TreeBucket::Node *TreeBucket::Find(Node *subtree_root, ClusterTreeNodeLabel const &node_label) {
+    assert(subtree_root != nullptr);
+
+    if (subtree_root->External()) {
+        if (!subtree_root->Empty() && subtree_root->Label() == node_label) {
+            return subtree_root;
+        } else {
+            return nullptr;
+        }
+    }
+
+    assert(subtree_root->Internal());
+
+    Node *result = this->Find(subtree_root->LeftChild(), node_label);
+    if (result != nullptr) {
+        return result;
+    }
+
+    return this->Find(subtree_root->RightChild(), node_label);
+}
+
+unsigned TreeBucket::TreeCapacity() const { return 1 << depth_; }
+
+void TreeBucket::BuildSubtree(Node *parent_node, unsigned parent_depth, unsigned target_depth) {
+    if (parent_depth == target_depth) {
+        return;
+    }
+
+    parent_node->SetChildren(
+        std::make_unique<Node>(parent_node, parent_depth + 1, /*left_child=*/true),
+        std::make_unique<Node>(parent_node, parent_depth + 1, /*left_child=*/false));
+
+    this->BuildSubtree(parent_node->LeftChild(), parent_depth + 1, target_depth);
+    this->BuildSubtree(parent_node->RightChild(), parent_depth + 1, target_depth);
+}
+
+void TreeBucket::ExpandDepth() {
+    auto old_root = std::move(root_);
+    auto new_root = std::make_unique<Node>(old_root.get());
+
+    // The old tree becomes the left subtree.
+    std::unique_ptr<Node> left_subtree = std::move(old_root);
+
+    // The right subtree is going to build entirely with empty nodes.
+    std::unique_ptr<Node> right_subtree =
+        std::make_unique<Node>(new_root.get(), /*depth=*/1, /*left_child=*/false);
+
+    this->BuildSubtree(right_subtree.get(), /*parent_depth=*/1, depth_ + 1);
+
+    // Combines subtrees and the new root is complete.
+    new_root->SetChildren(std::move(left_subtree), std::move(right_subtree));
+    root_ = std::move(new_root);
+
+    ++depth_;
+}
+
+TreeBucket::Node *TreeBucket::EmptyNodeFrom(Node *subtree_root) {
+    if (subtree_root->External()) {
+        if (subtree_root->Empty()) {
+            return subtree_root;
+        } else {
+            return nullptr;
+        }
+    }
+
+    assert(subtree_root->Internal());
+
+    Node *result = this->EmptyNodeFrom(subtree_root->LeftChild());
+    if (result != nullptr) {
+        return result;
+    }
+
+    return this->EmptyNodeFrom(subtree_root->RightChild());
+}
+
+std::optional<ClusterTreeNodeLabel> TreeBucket::Select(ResourceDescriptor const &resource,
+                                                       unsigned rank, unsigned num_failures) const {
+    if (root_->ExternalNodeCount() == 0) {
+        return std::nullopt;
+    }
+
+    // Randomly descent through the tree until we reach an external node.
+    Node *current_node = root_.get();
+
+    std::string const jumps = std::to_string(rank + num_failures);
+    std::string const key_and_jumps = resource.key + jumps;
+
+    while (current_node->Internal()) {
+        // We randomly selects a branch to descent based on each branch's hardware capability
+        // score with respect to the resource. It should distribute the resource fairly into the
+        // appropiate child. All internal nodes must have two child branches, though one of the
+        // branches may not have any external node. In this case, we should deterministically
+        // skip that branch.
+        if (current_node->LeftChild()->ExternalNodeCount() > 0 &&
+            current_node->RightChild()->ExternalNodeCount() > 0) {
+            std::vector<float> scores = scorer_->Score({current_node->LeftChild()->Capabilities(),
+                                                        current_node->RightChild()->Capabilities()},
+                                                       resource);
+            if (scores.empty()) {
+                return std::nullopt;
+            }
+
+            std::string hash_key = key_and_jumps + current_node->Id();
+            size_t random_variable = std::hash<std::string>()(hash_key);
+
+            if (EnterWithProbability(random_variable, scores[0])) {
+                current_node = current_node->LeftChild();
+            } else {
+                current_node = current_node->RightChild();
+            }
+        } else if (current_node->LeftChild()->ExternalNodeCount() > 0) {
+            if (scorer_->Score({current_node->LeftChild()->Capabilities()}, resource).empty()) {
+                return std::nullopt;
+            }
+
+            current_node = current_node->LeftChild();
+        } else if (current_node->RightChild()->ExternalNodeCount() > 0) {
+            if (scorer_->Score({current_node->RightChild()->Capabilities()}, resource).empty()) {
+                return std::nullopt;
+            }
+
+            current_node = current_node->RightChild();
+        } else {
+            // If the parent has non-zero external node count, one of the children must also has
+            // non-zero external node count.
+            assert(false);
+        }
+    }
+
+    assert(current_node->External());
+    assert(!current_node->Empty());
+    return current_node->Label();
+}
+
+bool TreeBucket::AddChild(ClusterTreeNodeLabel const &child_label) {
+    Node *old_node = this->Find(root_.get(), child_label);
+    if (old_node != nullptr) {
+        return false;
+    }
+
+    if (root_->ExternalNodeCount() == this->TreeCapacity()) {
+        // We've reached the tree's maximum capacity.
+        this->ExpandDepth();
+        assert(this->TreeCapacity() > root_->ExternalNodeCount());
+    }
+
+    Node *new_node = this->EmptyNodeFrom(root_.get());
+    assert(new_node != nullptr);
+    new_node->Use(child_label);
+
+    return true;
+}
+
+bool TreeBucket::RemoveChild(ClusterTreeNodeLabel const &child_label) {
+    Node *to_be_removed = this->Find(root_.get(), child_label);
+    if (to_be_removed == nullptr) {
+        return false;
+    }
+
+    // Only clears the node and not touch the tree topology.
+    to_be_removed->Clear();
+
+    return true;
+}
+
+std::vector<BucketInterface::Child> TreeBucket::Children() const {
+    std::vector<BucketInterface::Child> children;
+    if (root_ == nullptr) {
+        return children;
+    }
+
+    this->CollectBucketChildren(root_.get(), &children);
+    return children;
+}
+
+void TreeBucket::AddCapabilitiesFor(ClusterTreeNodeLabel const &child_label,
+                                    WeightedCapabilities const &delta) {
+    // Takes out the desired external node and updates the capability values for it as well as all
+    // its ancestors.
+    Node *node = this->Find(root_.get(), child_label);
+    assert(node != nullptr);
+
+    node->AddCapabilities(delta);
+}
+
+Bucket TreeBucket::ToProto() const {
+    Bucket bucket;
+
+    for (auto const &child : this->Children()) {
+        bucket.mutable_tree_bucket()->add_child_labels(child.label);
+    }
+
+    return bucket;
+}
+
 std::unique_ptr<BucketInterface> CreateBucket(Bucket const &proto,
                                               std::unique_ptr<CapabilityScoreInterface> &&scorer) {
     switch (proto.children_case()) {
@@ -253,8 +584,7 @@ std::unique_ptr<BucketInterface> CreateBucket(Bucket const &proto,
         return std::make_unique<ListBucket>(proto.list_bucket(), std::move(scorer));
     }
     case Bucket::ChildrenCase::kTreeBucket: {
-        // TODO: Implements tree bucket.
-        assert(false);
+        return std::make_unique<TreeBucket>(proto.tree_bucket(), std::move(scorer));
     }
     case Bucket::ChildrenCase::kStrawBucket: {
         // TODO: Implements straw bucket.
--- cluster/placement/bucket.h
+++ cluster/placement/bucket.h
@@ -21,6 +21,7 @@
 #include <list>
 #include <memory>
 #include <optional>
+#include <string>
 #include <vector>
 
 #include "cluster/placement/common_types.h"
@@ -99,10 +100,10 @@ class BucketInterface {
 };
 
 /**
- * @brief The UniformBucket class Distributes resources uniformly across the children without
- * consideration of their hardware capabilities. It's capable of achieving O(1) time select.
- * However, adding or removing any children will result in a complete reshuffling of the resources
- * that live under this bucket. This class isn't thread safe.
+ * @brief The UniformBucket class It distributes resources uniformly across the children without
+ * considering their hardware capabilities. It's capable of achieving O(1) time select. However,
+ * adding or removing any children will result in a complete reshuffling of the resources that live
+ * under this bucket. This class isn't thread safe.
  */
 class UniformBucket : public BucketInterface {
   public:
@@ -129,17 +130,18 @@ class UniformBucket : public BucketInterface {
 };
 
 /**
- * @brief The ListBucket class Distributes resources according to the distribution of the children's
- * hardware capabilities. It takes O(n) time to select a tree node for a bucket with n children.
- * Adding children requires optimal resource movement. However, removing children in the middle of
- * the list results in large amount of shuffling. This class isn't thread safe.
+ * @brief The ListBucket class A list bucket organizes child elements into a linked list and scans
+ * through the list to place a resource. So, it's capable of distributing resources according to
+ * children's hardware capabilities. It takes O(n) time to select a child item with n children.
+ * Adding children is optimally in terms of resource movement. However, removing children in the
+ * middle of the list results in large amount of shuffling. This class isn't thread safe.
  */
 class ListBucket : public BucketInterface {
   public:
     /**
      * @brief ListBucket Constructs a list bucket from the ListBucketData proto and a custom scorer.
-     * The scorer helps convert children's capabilities to a distribution which the Select()
-     * function places a resource pseudo-randomly with.
+     * The scorer helps convert children's capabilities with respect to a particular resource to a
+     * distribution which the Select() function places a resource pseudo-randomly with.
      */
     ListBucket(ListBucketData const &data, std::unique_ptr<CapabilityScoreInterface> &&scorer);
     ~ListBucket() override;
@@ -162,6 +164,173 @@ class ListBucket : public BucketInterface {
     std::vector<ClusterTreeNodeLabel> child_labels_;
     std::vector<WeightedCapabilities> children_capabilities_;
     std::unique_ptr<CapabilityScoreInterface> scorer_;
+};
+
+/**
+ * @brief The TreeBucket class A tree bucket organizes child elements in a cluster map into a
+ * balanced binary tree. This avoids the need to scan through a list of child elements while still
+ * be able to distribute resources fairly based on children's hardware capabilities. The selection
+ * process takes O(log(n)) time where n is the number of child elements in the bucket. Adding and
+ * removing child elements doesn't lead to catastrophic amount of shuffling.
+ */
+class TreeBucket : public BucketInterface {
+  public:
+    /**
+     * @brief TreeBucket Constructs a tree bucket from the TreeBucketData proto and a custom
+     * scorer. The scorer helps convert children's capabilities with respect to a particular
+     * resource to a distribution which the Select() function places a resource pseudo-randomly
+     * with.
+     */
+    TreeBucket(TreeBucketData const &data, std::unique_ptr<CapabilityScoreInterface> &&scorer);
+    ~TreeBucket();
+
+    std::optional<ClusterTreeNodeLabel> Select(ResourceDescriptor const &resource, unsigned rank,
+                                               unsigned num_failures) const override;
+
+    bool AddChild(ClusterTreeNodeLabel const &child_label) override;
+
+    bool RemoveChild(ClusterTreeNodeLabel const &child_label) override;
+
+    std::vector<Child> Children() const override;
+
+    void AddCapabilitiesFor(ClusterTreeNodeLabel const &child_label,
+                            WeightedCapabilities const &delta) override;
+
+    Bucket ToProto() const override;
+
+  private:
+    class Node {
+      public:
+        /**
+         * @brief Node Constructs the intial root node. This can only be called once for each tree.
+         */
+        Node();
+
+        /**
+         * @brief Node Constructs a new root on top of the old root and points the old root's parent
+         * to this node.
+         */
+        Node(Node *old_root);
+
+        /**
+         * @brief Node Constructs a non-root node under the specified parent.
+         */
+        Node(Node *parent, unsigned depth, bool left_child);
+
+        /**
+         * @brief Id Unique (over the entire tree) identifier of this node.
+         */
+        std::string Id() const;
+
+        /**
+         * @brief Internal Checks if this node is an internal node.
+         */
+        bool Internal() const;
+
+        /**
+         * @brief External Checks if this node is an external node.
+         */
+        bool External() const;
+
+        /**
+         * @brief SetChildren Sets the children of this node and turns it into an internal node.
+         * For an internal node, neither children coud be nullptr. The node must be an external
+         * node, or it will fail.
+         */
+        void SetChildren(std::unique_ptr<Node> &&left, std::unique_ptr<Node> &&right);
+
+        /**
+         * @brief Empty Checks if this node was marked as empty. The node must be an external node,
+         * or it will fail.
+         */
+        bool Empty() const;
+
+        /**
+         * @brief SetEmpty Clears the node's label to mark it as empty. Deducts capabilities and
+         * external node count from still-valid ancestors. The tree structure is left intact and
+         * this deleted node object serves as a placeholder so it could be re-used by the next
+         * SetUsed() call. The node must be an external node, or it will fail.
+         */
+        void Clear();
+
+        /**
+         * @brief Use Marks the tree as non-empty and labels it with the specified label value, then
+         * adds external node count to all its ancestors. The node must be an external node, or it
+         * will fail.
+         */
+        void Use(ClusterTreeNodeLabel const &label);
+
+        /**
+         * @brief AddCapabilities Adds the capability deltas to the node and all its ancestors. The
+         * node must be an external node, or it will fail.
+         */
+        void AddCapabilities(WeightedCapabilities const &delta);
+
+        /**
+         * @brief ExternalNodeCount Returns the number of non-empty external nodes in the subtree
+         * rooted by this node.
+         */
+        unsigned ExternalNodeCount() const;
+
+        /**
+         * @brief Label Returns the label of this node. This function will fail if the node is
+         * empty.
+         */
+        ClusterTreeNodeLabel Label() const;
+
+        /**
+         * @brief Capabilities The hardware capabilities of the subtree with this node being the
+         * root.
+         */
+        WeightedCapabilities Capabilities() const;
+
+        /**
+         * @brief LeftChild Pointer to the left child.
+         */
+        Node *LeftChild();
+
+        /**
+         * @brief RightChild Pointer to the right child.
+         */
+        Node *RightChild();
+
+        /**
+         * @brief LeftChild Pointer to the left child.
+         */
+        Node const *LeftChild() const;
+
+        /**
+         * @brief RightChild Pointer to the right child.
+         */
+        Node const *RightChild() const;
+
+      private:
+        std::string id;
+        std::optional<ClusterTreeNodeLabel> label;
+        WeightedCapabilities capabilities;
+
+        unsigned num_external_nodes;
+
+        Node *parent;
+        std::unique_ptr<Node> left;
+        std::unique_ptr<Node> right;
+    };
+
+    void CollectBucketChildren(Node const *node, std::vector<Child> *children) const;
+
+    Node *Find(Node *subtree_root, ClusterTreeNodeLabel const &node_label);
+
+    unsigned TreeCapacity() const;
+
+    void BuildSubtree(Node *parent_node, unsigned parent_depth, unsigned target_depth);
+
+    void ExpandDepth();
+
+    Node *EmptyNodeFrom(Node *subtree_root);
+
+    std::unique_ptr<CapabilityScoreInterface> scorer_;
+    std::unique_ptr<Node> root_;
+    unsigned depth_;
 };
 
 /**
