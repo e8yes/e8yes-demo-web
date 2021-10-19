ffd8c9c236f1c0d2142c2b3c31db363b5407f600
Chifeng Wen <daviesx66@gmail.com>
2021-10-19 02:39:39 -0700

    Adds a local cluster module for integration testing purposes.

---
+++ cluster/conductor/integration_test/local_cluster.cc
@@ -0,0 +1,118 @@
+/**
+ * e8yes demo web.
+ *
+ * <p>Copyright (C) 2020 Chifeng Wen {daviesx66@gmail.com}
+ *
+ * <p>This program is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * <p>This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * <p>You should have received a copy of the GNU General Public License along with this program. If
+ * not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <cassert>
+#include <google/protobuf/repeated_field.h>
+#include <grpcpp/grpcpp.h>
+#include <memory>
+#include <string>
+#include <utility>
+#include <vector>
+
+#include "cluster/conductor/instance.h"
+#include "cluster/conductor/integration_test/local_cluster.h"
+#include "cluster/conductor/integration_test/mock_resource_service.h"
+#include "cluster/placement/common_types.h"
+#include "replication/raft/common_types.h"
+#include "replication/runner/client.h"
+#include "replication/runner/runner.h"
+
+namespace e8 {
+namespace {
+
+constexpr char const *kTestHost = "localhost";
+unsigned const kTestCondutorPortBeginRange = 12345;
+unsigned const kTestWorkerPortBeginRange = 22345;
+constexpr char const *kTestLogFilePrefix = "local_raft_log";
+
+RaftMachineAddress GenerateConductorAddress(unsigned i) {
+    return std::string(kTestHost) + ":" + std::to_string(kTestCondutorPortBeginRange + i);
+}
+
+ResourceWorkerAddress GenerateResourceWorkerAddress(unsigned i) {
+    return std::string(kTestHost) + ":" + std::to_string(kTestWorkerPortBeginRange + i);
+}
+
+std::string LogPath(RaftMachineAddress const node_address) {
+    return std::string("./") + kTestLogFilePrefix + "." + node_address;
+}
+
+void StartConductors(unsigned num_conductors,
+                     std::vector<LocalCluster::ConductorNode> *conductors) {
+    google::protobuf::RepeatedPtrField<RaftMachineAddress> conductor_addresses;
+    for (unsigned i = 0; i < num_conductors; ++i) {
+        conductor_addresses.Add(GenerateConductorAddress(i));
+    }
+
+    for (unsigned i = 0; i < num_conductors; ++i) {
+        LocalCluster::ConductorNode node;
+        node.address = conductor_addresses[i];
+
+        node.config.set_fulfillment_timeout_millis(100);
+        node.config.mutable_raft_config()->set_me(conductor_addresses[i]);
+        node.config.mutable_raft_config()->set_log_path(LogPath(conductor_addresses[i]));
+        node.config.mutable_raft_config()->set_quorum_size(num_conductors / 2 + 1);
+        node.config.mutable_raft_config()->set_unavailability(1.0f);
+        *node.config.mutable_raft_config()->mutable_peers() = conductor_addresses;
+
+        node.conductor = std::make_unique<ConductorInstance>(node.config);
+
+        conductors->emplace_back(std::move(node));
+    }
+}
+
+void StartResourceWorkers(unsigned num_resource_workers,
+                          std::vector<LocalCluster::WorkerNode> *resource_workers) {
+    for (unsigned i = 0; i < num_resource_workers; ++i) {
+        LocalCluster::WorkerNode node;
+        node.address = GenerateResourceWorkerAddress(i);
+        node.service = std::make_unique<MockResourceService>();
+
+        grpc::ServerBuilder builder;
+        builder.AddListeningPort(node.address, grpc::InsecureServerCredentials());
+        builder.RegisterService(node.service.get());
+        node.server = builder.BuildAndStart();
+
+        resource_workers->emplace_back(std::move(node));
+    }
+}
+
+} // namespace
+
+LocalCluster::LocalCluster(unsigned num_conductors, unsigned num_resource_workers) {
+    assert(num_conductors > 0);
+
+    StartConductors(num_conductors, &conductors_);
+    StartResourceWorkers(num_resource_workers, &resource_workers_);
+}
+
+LocalCluster::~LocalCluster() {}
+
+ReplicationClientConfig LocalCluster::GetConductorClientConfig() {
+    ReplicationClientConfig config;
+    for (auto const &condutor : conductors_) {
+        config.peers.insert(condutor.address);
+    }
+    return config;
+}
+
+LocalCluster::WorkerNode const &LocalCluster::GetWorkerNode(unsigned i) const {
+    assert(i < resource_workers_.size());
+    return resource_workers_[i];
+}
+
+} // namespace e8
---
+++ cluster/conductor/integration_test/local_cluster.h
@@ -0,0 +1,103 @@
+/**
+ * e8yes demo web.
+ *
+ * <p>Copyright (C) 2020 Chifeng Wen {daviesx66@gmail.com}
+ *
+ * <p>This program is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * <p>This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * <p>You should have received a copy of the GNU General Public License along with this program. If
+ * not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef CLUSTER_CONDUCTOR_LOCAL_CLUSTER_H
+#define CLUSTER_CONDUCTOR_LOCAL_CLUSTER_H
+
+#include <grpcpp/grpcpp.h>
+#include <memory>
+#include <vector>
+
+#include "cluster/conductor/instance.h"
+#include "cluster/conductor/integration_test/mock_resource_service.h"
+#include "cluster/placement/common_types.h"
+#include "replication/raft/common_types.h"
+#include "replication/runner/client.h"
+#include "replication/runner/runner.h"
+
+namespace e8 {
+
+/**
+ * @brief The LocalCluster class Manages a testing cluster consisting of locally running conductor
+ * instances and resource service workers.
+ */
+class LocalCluster {
+  public:
+    /**
+     * @brief The ConductorNode struct The client of this structure should only modify the conductor
+     * instance. Other fields are readonly.
+     */
+    struct ConductorNode {
+        // The IP address of the conductor node which houses the conductor instance.
+        RaftMachineAddress address;
+
+        // The configuration used to start the conductor instance.
+        ReplicationConfig config;
+
+        // The running conductor instance if not null.
+        std::unique_ptr<ConductorInstance> conductor;
+    };
+
+    /**
+     * @brief The WorkerNode struct The client of this structure should only interact upon the
+     * service. Other fields are readonly.
+     */
+    struct WorkerNode {
+        // The IP address of the resource worker service.
+        ResourceWorkerAddress address;
+
+        // The mock resource worker service for testing, if not null.
+        std::unique_ptr<MockResourceService> service;
+
+        // The running server providing the resource worker service.
+        std::unique_ptr<grpc::Server> server;
+    };
+
+    /**
+     * @brief LocalCluster Constructs a cluster running locally for testing purposes.
+     *
+     * @param num_conductors Specifies the number of conductor nodes in the cluster. Must be greater
+     * than 0.
+     * @param num_resource_workers Specifies the number of resource workers in the cluster. Could be
+     * zero.
+     */
+    LocalCluster(unsigned num_conductors, unsigned num_resource_workers);
+    ~LocalCluster();
+
+    /**
+     * @brief GetConductorClientConfig Returns the config that can be used to build a conductor
+     * client. The built client will interact with all the conductor nodes in the cluster, whether
+     * they are alive or not.
+     */
+    ReplicationClientConfig GetConductorClientConfig();
+
+    /**
+     * @brief GetWorkerNode Gets a worker node by a zero-offset index.
+     *
+     * @param i Must be in the interval [0, num_resource_workers).
+     * @param A const reference to the worker node with index i.
+     */
+    WorkerNode const &GetWorkerNode(unsigned i) const;
+
+  private:
+    std::vector<ConductorNode> conductors_;
+    std::vector<WorkerNode> resource_workers_;
+};
+
+} // namespace e8
+
+#endif // CLUSTER_CONDUCTOR_LOCAL_CLUSTER_H
