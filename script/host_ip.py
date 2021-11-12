b735f26245f1f715a890d25d22b8999928b5913b
Chifeng Wen <daviesx66@gmail.com>
2021-11-12 03:46:07 -0800

    These scripts are useless.

--- script/host_ip.py
+++
@@ -1,7 +0,0 @@
-import subprocess
-
-def GetHostIp(location: str) -> str:
-  output = subprocess.check_output(["getent", "hosts", location])
-  output = output[:-1].decode("utf-8")
-  parts = output.split(" ")
-  return parts[0]
